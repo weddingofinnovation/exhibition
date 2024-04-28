fc73374d4c939e7cf203ba28f104d73dbf1dc3bd
lara <laravel8coi@gmail.com>
2024-04-28 20:49:21 +0530

    28apr20240849

--- resources/views/livewire/exhibit-component.blade.php
+++ resources/views/livewire/exhibit-component.blade.php
@@ -47,38 +47,45 @@
         @endif
 
         @if($board == 'registration-great-exhibition-to-visit')
-            <form  wire:submit.prevent="add" >
-              <div class="mb-3">
-                <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
-                <input class="form-control" type="name"  wire:model.lazy="name" required="" placeholder="Enter your name">
-                <div class="invalid-feedback">Please provide valid email address.</div>
-              </div>
+          <div class="container py-4 py-lg-5 my-4">
+            <div class="row justify-content-center">
+              <div class="col-lg-8 col-md-10">
+                <h2 class="h3 mb-2">Registration</h2>
+                <form  wire:submit.prevent="add" >
+                  <div class="mb-3">
+                    <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                    <input class="form-control" type="name"  wire:model.lazy="name" required="" placeholder="Enter your name">
+                    <div class="invalid-feedback">Please provide valid email address.</div>
+                  </div>
 
-              <div class="mb-3">
-                <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
-                <input class="form-control" type="name"  wire:model.lazy="designation" required="" placeholder="Enter your designation">
-                <div class="invalid-feedback">Please provide valid email address.</div>
-              </div>
+                  <div class="mb-3">
+                    <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                    <input class="form-control" type="name"  wire:model.lazy="designation" required="" placeholder="Enter your designation">
+                    <div class="invalid-feedback">Please provide valid email address.</div>
+                  </div>
 
-              <div class="mb-3">
-                <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
-                <input class="form-control" type="email"  wire:model.lazy="email" required="" placeholder="Enter your email address">
-                <div class="invalid-feedback">Please provide valid email address.</div>
-              </div>
-              <div class="mb-3">
-                <!-- <label class="form-label" for="recover-email">Enter your phone</label> -->
-                <input class="form-control" type="number"  wire:model.lazy="phone" required="" placeholder="Enter your phone">
-                <div class="invalid-feedback">Please provide valid phone.</div>
-              </div>
+                  <div class="mb-3">
+                    <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                    <input class="form-control" type="email"  wire:model.lazy="email" required="" placeholder="Enter your email address">
+                    <div class="invalid-feedback">Please provide valid email address.</div>
+                  </div>
+                  <div class="mb-3">
+                    <!-- <label class="form-label" for="recover-email">Enter your phone</label> -->
+                    <input class="form-control" type="number"  wire:model.lazy="phone" required="" placeholder="Enter your phone">
+                    <div class="invalid-feedback">Please provide valid phone.</div>
+                  </div>
 
-              <div class="mb-3">
-                <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
-                <input class="form-control" type="name"  wire:model.lazy="city" required="" placeholder="Enter your city">
-                <div class="invalid-feedback">Please provide valid email address.</div>
-              </div>
+                  <div class="mb-3">
+                    <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                    <input class="form-control" type="name"  wire:model.lazy="city" required="" placeholder="Enter your city">
+                    <div class="invalid-feedback">Please provide valid email address.</div>
+                  </div>
 
-              <button class="btn btn-primary" type="submit">Submit</button>
-            </form>
+                  <button class="btn btn-primary" type="submit">Submit</button>
+                </form>
+              </div>
+            </div>
+          </div>
         @endif
 
         @if($board == 'ticket')
