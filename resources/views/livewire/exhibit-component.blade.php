764047c982c220b8f49a594318b24ec0b2e77b06
lara <laravel8coi@gmail.com>
2024-05-04 22:15:57 +0530

    04may20241015

--- resources/views/livewire/exhibit-component.blade.php
+++ resources/views/livewire/exhibit-component.blade.php
@@ -492,7 +492,7 @@
           <div class="container py-4 py-lg-5 my-4">
             <div class="row justify-content-center">
               <div class="col-lg-8 col-md-10">
-                <h5 class=" mb-2 text-center">Thank you for registering for <strong>{{$findevent->eventname}}</strong>. Your registration details have been received.</h5>
+                <h5 class=" mb-2 text-center">Thank you {{$findvisitor->name}} for registering for <strong>{{$findevent->eventname}}</strong>. Your registration details have been received.</h5>
                 <!-- <p class="fs-md">Join the ranks of top industry leaders and connect with your target audience. <br> Thanks for sharing and let's make it happen </p>
                 <ol class="list-unstyled fs-md">
                   <li><span class="text-primary me-2"></span>Thank you for your interest in the Exhibition</li>
@@ -502,21 +502,22 @@
                 @php 
                    $findvisitor = DB::table('leads')->where('id', $visitorid)->first();
                 @endphp
-<!-- 
-                   {{$findvisitor->company}}
-                  <h1>{{$findvisitor->name}}</h1>
-                  <h3>{{$findvisitor->designation}}</h3> -->
-                  
-                  
-                <div class="container d-flex  justify-content-center">
-                {{QrCode::size(190)->generate('it is registered visitor')}}
-                </div>
+
               </div>
             </div>
-            
           </div>
-          <div class="d-flex  justify-content-end">
-                <a class="btn btn-primary btn-sm" href="#" wire:click.prevent="savecont({{$visitorid}})">Add Your Expo Profile</a>
+          @if(Auth::check())
+            <a href="{{route('user.dashboard')}}" >Add your profile</a>
+          @else
+          <a href="{{route('login')}}" >Add your profile</a>
+          @endif
+
+          <div class="d-flex  justify-content-sm-end">
+            @if(Auth::check())
+                <a class="btn btn-primary btn-sm" href="#" wire:click.prevent="savecont({{$visitorid}})">Add Your Contact</a>
+            @else
+                <a class="btn btn-primary btn-sm" href="#" wire:click.prevent="savecont({{$visitorid}})">Add Your Contact</a>
+            @endif
                 <a class="btn btn-primary btn-sm" href="#" wire:click.prevent="genratepdf({{$visitorid}})">Download</a>
             </div>
         @endif
