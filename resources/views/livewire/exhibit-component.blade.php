befc94e3b3df93548d124415523c72e3f5ee2b6b
lara <laravel8coi@gmail.com>
2024-05-05 13:02:47 +0530

    05may20240102

--- resources/views/livewire/exhibit-component.blade.php
+++ resources/views/livewire/exhibit-component.blade.php
@@ -489,19 +489,20 @@
         @endif
 
         @if($board == 'thankyou')
+                @php 
+                   $findvisitor = DB::table('leads')->where('id', $visitorid)->first();
+                @endphp
           <div class="container py-4 py-lg-5 my-4">
             <div class="row justify-content-center">
               <div class="col-lg-8 col-md-10">
-                <h5 class=" mb-2 text-center">Thank you {{$findvisitor->name}} for registering for <strong>{{$findevent->eventname}}</strong>. Your registration details have been received.</h5>
+                <div class=" mb-2 text-center">Thank you {{$findvisitor->name}} for registering for <strong>{{$findevent->eventname}}</strong>. Your registration details have been received.</div>
                 <!-- <p class="fs-md">Join the ranks of top industry leaders and connect with your target audience. <br> Thanks for sharing and let's make it happen </p>
                 <ol class="list-unstyled fs-md">
                   <li><span class="text-primary me-2"></span>Thank you for your interest in the Exhibition</li>
                   <li><span class="text-primary me-2"></span>Our representative will be contacting you shortly to discuss your participation.</li>
                   <li><span class="text-primary me-2"></span>If you prefer, you can also proactively reach us at 9991856776</li>
                 </ol>-->
-                @php 
-                   $findvisitor = DB::table('leads')->where('id', $visitorid)->first();
-                @endphp
+                
 
               </div>
             </div>
