36730d2a0dbaee55064d239a75ce31a725f6a7b0
lara <laravel8coi@gmail.com>
2024-05-05 20:32:03 +0530

    05may20240831

--- resources/views/livewire/document/expand-component.blade.php
+++ resources/views/livewire/document/expand-component.blade.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <title>Document</title>
 </head>
     <body>
             <div class="container">
-                <div class="d-flex  justify-content-between">
+                <div class="d-flex  justify-content-center">
                     {{QrCode::size(255)->generate('thanks for it!!')}}
-                    <h1>{{$company}}</h1>
-                    <h1>{{$name}}</h1>
-                    <h1>{{$designation}}</h1>
+                    <h1>{{$wantdata->company}}</h1>
+                    <h1>{{$wantdata->name}}</h1>
+                    <h1>{{$wantdata->designation}}</h1>
                 </div>
             </div>
     </body>
