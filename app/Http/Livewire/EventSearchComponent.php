fc028cbbf7df38d28dfe76d1661744c21eacfa6b
lara <laravel8coi@gmail.com>
2024-05-14 13:53:41 +0530

    14may20240153

--- app/Http/Livewire/EventSearchComponent.php
+++ app/Http/Livewire/EventSearchComponent.php
@@ -40,23 +40,7 @@ class EventSearchComponent extends Component
     {
         $mytime = Carbon::now();
         
-        if($this->country != null )
-         {
-            //$searchVenue = Event::where('country', $this->country)->where('status', '1')->where('admstatus', '1')->orderBy('startdate','desc')->get();
-            $searchTermCountry = '%'.$this->country. '%';
-            $searchVenue = Event::Where('country','LIKE', $searchTermCountry)->where('status','1')->orderBy('eventname','ASC')->get();
-         }
-        
-        elseif($this->venue != null)
-        {
-            //$searchTermVenue = '%'.$this->venue. '%';
-            $searchVenue = Event::where('venue', $this->venue)->where('status', '1')->where('admstatus', '1')->orderBy('startdate','desc')->get();
-        }
-        elseif($this->city != null)
-        {
-            $searchTermCity = '%'.$this->city. '%';
-            $searchVenue = Event::where('city', $searchTermCity )->where('status', '1')->where('admstatus', '1')->orderBy('startdate','desc')->get();
-        }
+        $searchVenue = Event::where('venue', $this->venue)->where('status', '1')->where('admstatus', '1')->orderBy('startdate','desc')->get();
          
         $mytime = Carbon::now();
         
