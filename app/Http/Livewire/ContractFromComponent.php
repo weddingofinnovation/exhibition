f0897efcf8ddf377667f31664b59ff179f687a70
lara <laravel8coi@gmail.com>
2024-05-16 10:33:10 +0530

    16may20241033

--- app/Http/Livewire/ContractFromComponent.php
+++ app/Http/Livewire/ContractFromComponent.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Contractio;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class ContractFromComponent extends Component
 {
@@ -33,8 +34,6 @@ class ContractFromComponent extends Component
        $finder = Contractio::find($brand_id);
        $this->brand_id = $finder->id;
        $this->owner = $finder->owner; 
-      
-       
         $this->organisation = $finder->organisation;
         $this->brand_name = $finder->brand_name;
         $this->GST = $finder->GST;
@@ -57,21 +56,21 @@ class ContractFromComponent extends Component
     {
         $contractformi = Contractio::find($this->brand_id);
 
-        $contractformi->owner = $this->owner;
-        $contractformi->organisation = $this->organisation;
-        $contractformi->brand_name = $this->brand_name;
-        $contractformi->GST = $this->GST;
-        $contractformi->industry = $this->industry;
-        $contractformi->product = $this->product;
-        $contractformi->email = $this->email;
-        $contractformi->phone = $this->phone;
-        $contractformi->address = $this->address;
-        $contractformi->city = $this->city;
-        $contractformi->state = $this->state;
-        $contractformi->country = $this->country;
-        $contractformi->hall = $this->hall;
-        $contractformi->stall = $this->stall;
-        $contractformi->size = $this->size;
+        $contractformi->owner = Str::lower(trim($this->owner));
+        $contractformi->organisation = Str::lower(trim($this->organisation));
+        $contractformi->brand_name = Str::lower(trim($this->brand_name));
+        $contractformi->GST = Str::lower(trim($this->GST));
+        $contractformi->industry = Str::lower(trim($this->industry));
+        $contractformi->product = Str::lower(trim($this->product));
+        $contractformi->email = Str::lower(trim($this->email));
+        $contractformi->phone = Str::lower(trim($this->phone));
+        $contractformi->address = Str::lower(trim($this->address));
+        $contractformi->city = Str::lower(trim($this->city));
+        $contractformi->state = Str::lower(trim($this->state));
+        $contractformi->country = Str::lower(trim($this->country));
+        $contractformi->hall = Str::lower(trim($this->hall));
+        $contractformi->stall = Str::lower(trim($this->stall));
+        $contractformi->size = Str::lower(trim($this->size));
         
         //$contractformi->brand_id = '1';
         //$contractformi->event_id = '2';
--- resources/views/livewire/contract-from-component.blade.php
+++ resources/views/livewire/contract-from-component.blade.php
@@ -4,17 +4,27 @@
     <div class="container">
         @if($formm == 'basic')
             <form wire:submit.prevent="contractForm">
-                <h1>Exhibitor Details</h1>
-                <input type="text" class="form-control" placeholder="owner" required wire:model.lazy="owner">
-                <input type="text" class="form-control" placeholder="company" required wire:model.lazy="organisation">
-                <input type="text" class="form-control" placeholder="brand" required wire:model.lazy="brand_name">
-                <input type="text" class="form-control" placeholder="pan" required wire:model.lazy="GST">
-                <input type="text" class="form-control" placeholder="Industry" required wire:model.lazy="industry">
-                <input type="text" class="form-control" placeholder="product" required wire:model.lazy="product">
+
+
+                <h1>start</h1>
                 <input type="text" class="form-control" placeholder="email" required wire:model.lazy="email">
                 <input type="text" class="form-control" placeholder="contact" required wire:model.lazy="phone">
 
+                <h1>Exhibitor Details</h1>
+
+                <input type="text" class="form-control" placeholder="owner" required wire:model.lazy="owner">
+                <input type="text" class="form-control" placeholder="company" required wire:model.lazy="organisation">
+                <input type="text" class="form-control" placeholder="brand" required wire:model.lazy="brand_name">
+                <input type="text" class="form-control" placeholder="gst" required wire:model.lazy="GST">
+
+                
+                <h1>Category</h1>
+                <input type="text" class="form-control" placeholder="Industry" required wire:model.lazy="industry">
+                <input type="text" class="form-control" placeholder="product" required wire:model.lazy="product">
+                
+
                 <h1>Address</h1>
+                <input type="text" class="form-control" placeholder="office_type" required wire:model.lazy="office_type">
                 <input type="text" class="form-control" placeholder="address" required wire:model.lazy="address">
                 <input type="text" class="form-control" placeholder="city" required wire:model.lazy="city">
                 <input type="text" class="form-control" placeholder="state" required wire:model.lazy="state">
@@ -25,10 +35,11 @@
                 <input type="text" class="form-control" placeholder="hall" required wire:model.lazy="hall">
                 <input type="text" class="form-control" placeholder="stall" required wire:model.lazy="stall">
                 <input type="text" class="form-control" placeholder="size" required wire:model.lazy="size">
-
+                <input type="text" class="form-control" placeholder="side_open" required wire:model.lazy="side_open">
 
                 <button class="btn btn-primary form-control" type="submit">Submit</button>
             </form>
+            
         @elseif($formm == 'payment')
             <h1>Calculation</h1> 
             <form wire:submit.prevent="payment">      
