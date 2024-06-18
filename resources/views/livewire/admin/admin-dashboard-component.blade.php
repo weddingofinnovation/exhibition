754308d7d996c3385ab2c82b0be7c7419964463d
lara <laravel8coi@gmail.com>
2024-06-18 21:41:59 +0530

    18june20240941

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -63,10 +63,10 @@
                   <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm active" href="#details" data-bs-toggle="tab" role="tab">Monthly {{$monthwise->count()}}</a></li>
                   <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#reviews" data-bs-toggle="tab" role="tab">Search {{$searchCat->count()}}</a></li>
                   <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#reviewID" data-bs-toggle="tab" role="tab">ID </a></li>
-                  <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#year22find" data-bs-toggle="tab" role="tab">2022 {{$year22ev->count}}</a></li>
-                  <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#year23find" data-bs-toggle="tab" role="tab">2023 {{$year23ev->count}}</a></li>
-                  <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#year24find" data-bs-toggle="tab" role="tab">2024 {{$year24ev->count}}</a></li>
-                  <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#year25find" data-bs-toggle="tab" role="tab">2025 {{$year25ev->count}}</a></li>
+                  <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#year22find" data-bs-toggle="tab" role="tab">2022 {{$year22ev->count()}}</a></li>
+                  <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#year23find" data-bs-toggle="tab" role="tab">2023 {{$year23ev->count()}}</a></li>
+                  <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#year24find" data-bs-toggle="tab" role="tab">2024 {{$year24ev->count()}}</a></li>
+                  <li class="nav-item border-bottom"><a class="nav-link px-1 fs-sm" href="#year25find" data-bs-toggle="tab" role="tab">2025 {{$year25ev->count()}}</a></li>
                 </ul>
 
                 {{--<div class="d-flex badgese pb-2">
