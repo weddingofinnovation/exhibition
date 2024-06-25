518c711bf4d39e6bd518034021de99600aefaff0
lara <laravel8coi@gmail.com>
2024-06-25 20:44:32 +0530

    25jun20240843

--- resources/views/livewire/footer-component.blade.php
+++ resources/views/livewire/footer-component.blade.php
@@ -758,11 +758,12 @@
               <div class="offcanvas-body py-grid-gutter px-lg-grid-gutter">
                 
                   @php  
-                      $locationo = DB::table('locations')->where('status', 1)->whereNotNull('venue')->select('city' , 'country')->groupBy('city')->orderBy('city','asc')->get();
+                      $locationo = DB::table('locations')->where('status', 1)->whereNotNull('venue')->select('city')->groupBy('city')->orderBy('city','asc')->get();
+                      $addcountry = $locationo->addSelect('country')->get();
                   @endphp
                   
                     <div class="grido">
-                      @foreach ($locationo as $category)
+                      @foreach ($addcountry as $category)
                         <a class="element-item" data-category="metalloid" href="{{route('search.venue',['time' => 'upcoming', 'venue' => 'all' , 'city' => $category->city, 'country' => $category->country ])}}">
                           <h3 class="name">{{ucwords(trans(Str::limit($category->city, 24)))}}</h3>
                           <p class="symbol"><i class="bi bi-list"></i></p>
