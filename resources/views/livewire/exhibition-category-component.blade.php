e9bc8317424dfbc8fe9eae00b9238782d72f28cb
lara <laravel8coi@gmail.com>
2024-06-29 21:34:45 +0530

    29june20240934

--- resources/views/livewire/exhibition-category-component.blade.php
+++ resources/views/livewire/exhibition-category-component.blade.php
@@ -1749,27 +1749,41 @@
           @endphp
 
           @foreach ($franchiso as $franchise)
-            <div class="offcanvas offcanvas-bottom" tabindex="-1" id="{{$franchise->slug}}" aria-labelledby="offcanvasExampleLabel" style="height: 285px;">
+            <div class="offcanvas offcanvas-bottom" tabindex="-1" id="{{$franchise->slug}}" aria-labelledby="offcanvasExampleLabel" style="height: 325px;">
             
-              <div class="offcanvas-header align-items-center shadow-sm">
+            <div class="offcanvas-header align-items-center shadow-sm">
                 <h2 class="h5 mb-0">{{$franchise->eventname}}</h2>
                 <button class="btn-close ms-auto" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
               </div>
-               
-              <div class="offcanvas-body py-grid-gutter px-lg-grid-gutter">
-                  
-                    <a href="#" target= "_blank"> pre-book </a>
-                    <a href="#" target= "_blank"> fabrication</a>
-                    <a href="#" target= "_blank"> Directory</a>
-                    <a href="#" target= "_blank"> Details</a>
-                    <a href="#" target= "_blank"> Membership</a>
-                    <a href="#" target= "_blank"> save </a>
-              </div>
               
+               
+                <div class="offcanvas-body py-grid-gutter px-lg-grid-gutter">
+                   
+                      <a href="http://">business</a>
+                    
+                </div>
+              
+            
+            
               <div class="handheld-toolbar">
 
                 <div class="d-table table-layout-fixed w-100">
                         
+                  <a class="d-table-cell handheld-toolbar-item" href="">
+                    <span class="handheld-toolbar-icon"><i class="bi bi-add"></i></span>
+                    <span class="handheld-toolbar-label">Exhibitor</span>
+                  </a>
+
+                  <a class="d-table-cell handheld-toolbar-item" href="{{route('admin.dashboard', ['board' => 'visitcard'])}}">
+                    <span class="handheld-toolbar-icon"><i class="bi bi-add"></i></span>
+                    <span class="handheld-toolbar-label">save</span>
+                  </a>
+
+                  <a class="d-table-cell handheld-toolbar-item" href="{{route('admin.dashboard', ['board' => 'visitcard'])}}">
+                    <span class="handheld-toolbar-icon"><i class="bi bi-add"></i></span>
+                    <span class="handheld-toolbar-label">fabrication</span>
+                  </a>
+
                   <a class="d-table-cell handheld-toolbar-item" href="{{route('admin.dashboard', ['board' => 'visitcard'])}}">
                     <span class="handheld-toolbar-icon"><i class="bi bi-add"></i></span>
                     <span class="handheld-toolbar-label">Membership</span>
