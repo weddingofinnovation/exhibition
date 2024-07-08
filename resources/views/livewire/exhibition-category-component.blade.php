476cabc462710fce254bb2b9e42ce107dd3d0487
lara <laravel8coi@gmail.com>
2024-07-08 22:50:23 +0530

    08jul20241050

--- resources/views/livewire/exhibition-category-component.blade.php
+++ resources/views/livewire/exhibition-category-component.blade.php
@@ -368,7 +368,7 @@
                                     </div>
                                   </div>
                                 </div>
-                            @endif
+                           
 
                           @endforeach
                         @endforeach
