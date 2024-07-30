aec7b5dad50a0de131e5569e62c6d1ee2dde1f8e
lara <laravel8coi@gmail.com>
2024-07-30 18:47:46 +0530

    30jul20240647

--- resources/views/livewire/user/user-badge-component.blade.php
+++ resources/views/livewire/user/user-badge-component.blade.php
@@ -1,29 +1,37 @@
 <main>
+    <div class="container">
+        @if($board == 'badge-application-form')
+            <div>submit your website get badge</div>
+            <form wire:submit.prevent="submit">
+                <input type="text" class="form-control" required wire:model="website_url" placeholder="Your Website URL">
 
-    @if($board = 'badge-application-form')
+                <select class="form-control" type="text"   wire:model.lazy="type"   placeholder="type">
+                            <option selected>Choose</option>
+                                <option value="strategic">Strategic Partner</option>
+                                <option value="space">Space Partner</option>
+                            
+                </select>
 
-        <form wire:submit.prevent="submit">
-            <input type="text" class="" required wire:model="website_url" placeholder="Your Website URL">
-            <input type="text" class="" required wire:model="type" placeholder="Your Website URL">
-            <button type="submit" class="btn btn-primary">Apply for Badge</button>
-        </form>
+                <button type="submit" class="btn btn-primary form-control">Apply for Badge</button>
+            </form>
 
-    @elseif($board = 'badge-code')
+        @elseif($board == 'badge-code')
 
-        <div>
-            @if($badgeCode)
-            <p>Your badge Code is: {{$badgeCode->code}}</p>
-            <p>To display our badge on your site, use the following code:</p>
-            <pre>
-                &lt;a href="https://yourwebsite.com" target="_blank" rel="noopener noreferrer"&gt;
-                &lt;img src="https://yourwebsite.com/path/to/badge/logo.png" alt="Trust Partner Badge" /&gt;
-                $lt;/a&gt;
-            </pre>
-            @else
-            <p>Invalid or Expired code.</p>
-            @endif
-        </div>
+            <div>
+                @if($badgeCode)
+                <p>Your badge Code is: {{$badgeCode->code}}</p>
+                <p>To display our badge on your site, use the following code:</p>
+                <pre>
+                    &lt;a href="https://yourwebsite.com" target="_blank" rel="noopener noreferrer"&gt;
+                    &lt;img src="https://yourwebsite.com/path/to/badge/logo.png" alt="Trust Partner Badge" /&gt;
+                    $lt;/a&gt;
+                </pre>
+                @else
+                <p>Invalid or Expired code.</p>
+                @endif
+            </div>
 
-    @endif
-
+        @endif
+    </div>
 </main>
+
