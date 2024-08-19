f4ea784d975dbdf346998d65a0570cd1a49faefa
lara <laravel8coi@gmail.com>
2024-08-19 14:48:23 +0530

    19aug20240247

--- config/services.php
+++ config/services.php
@@ -16,7 +16,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env ('https://exhibition.org.in/google/callback'),
+        'redirect' => env ('GOOGLE_REDIRECT_URI'),
     ],
 
     'linkedin' => [
