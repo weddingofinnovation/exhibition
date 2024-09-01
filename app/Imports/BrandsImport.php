a5859dd553412de7ecfb249e4a17d1da627a7ea3
lara <laravel8coi@gmail.com>
2024-09-01 21:55:44 +0530

    01sept20240955

--- app/Imports/BrandsImport.php
+++ app/Imports/BrandsImport.php
@@ -10,13 +10,16 @@ class BrandsImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
-        return Brand::updateOrCreate(
-            ['brand_name' => $row['exhibitor']],  // Assuming 'name' is a column in your CSV
-            [
-                'industry' => $row['industry'],  // Add other fields here
-                'location' => $row['location'],
-                // Add other fields as needed
-            ]
-        );
+        return new Brand([
+            'brand_name' => $row['1'], 
+        ]);
+        // return Brand::updateOrCreate(
+        //     ['brand_name' => $row['exhibitor']],  // Assuming 'name' is a column in your CSV
+        //     [
+        //         'industry' => $row['industry'],  // Add other fields here
+        //         'location' => $row['location'],
+        //         // Add other fields as needed
+        //     ]
+        // );
     }
 }
