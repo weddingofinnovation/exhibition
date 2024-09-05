44a2f89d75d2e2bf5c8354f626e564fc7c9a435a
lara <laravel8coi@gmail.com>
2024-09-05 20:36:44 +0530

    05sep20240836

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -1529,7 +1529,7 @@
                     }
                 </style>
                 <!-- fixedthis -->
-                <div class="container d-none d-sm-block fixed-bottom-div">
+                <div class="d-none d-sm-block fixed-bottom-div">
                   <ul class="list-unstyled fs-sm  py-2">     
                     <li class="d-flex justify-content-between p-0 m-0">
                         <span class="col bg-light mb-0">
