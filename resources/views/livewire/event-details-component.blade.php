c8f3a41667ce3528327457dad412f03c12f47115
lara <laravel8coi@gmail.com>
2024-10-04 11:27:43 +0530

    04oct20241127

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -1180,7 +1180,6 @@
             </div>
           </section>
          
-
           <!--share hidden-->
           <div class="d-none">            
                 <a href="#" id="gmail-btn">gmail</a>
@@ -1205,43 +1204,6 @@
                 </div>
               </section> -->
 
-              
-                    
-                  <!--exhibitor-->
-                    <hr class="mt-md-2 mb-2">
-                    <section class="container pt-2 pt-md-5">
-                      <h6 class="text-left mb-2"> Participants</h6>
-                      <div class="my-sliderexpo d-none d-sm-block">
-                            @foreach ($eventbrand as $franchise)
-                                <div class="col-sm-3 mb-grid-gutter">
-                                  <div class="card product-card-alt">
-                                          
-                                        <div class="product-thumb p-3">
-                                          
-                                          
-                                            <div class="product-card-actions p-2">
-                                            
-                                              <div class="fs-sm text-light" href="">Booth 3</div>
-                                              <div class="fs-sm text-light" href="">Elite Sponsor</div>
-                                              <div class="fs-sm text-light" href="">View Website</div>
-                                            </div>
-
-                                            <a class="product-thumb-overlay" href=""></a>
-
-                                            
-                                        </div>  
-                                  </div>
-                                </div>
-                            @endforeach
-                      </div>
-
-                      <div class="my-sliderexpo d-lg-none">
-                          <a class="d-flex align-items-center" href="#">
-                            
-                          </a>
-                      </div>
-                    </section>
-                  
                   <!-- Partner-->
                     <hr class="mt-md-2 mb-2">
                     <section class="container py-2 pt-md-5">
@@ -1265,112 +1227,113 @@
                       </div>
                     </section>
 
-                    @php 
-                $relativeEvent = DB::table('events')->where('reference' , $event->reference)->get();
+              @php 
+               $relativeEvent = DB::table('events')->where('reference' , $event->reference)->get();
               @endphp
 
               @if($relativeEvent->count() >= 2 )
-              
-                <div class="list-unstyled pt-2 pb-0 px-0 pl-0">
-                      <div class="d-flex justify-content-between px-0 m-0 lh-1 ">
-                        <span class="fs-sm"> 2Concurrent<br><span class="fw-medium h5">Event</span></span>
-                        <a class="btn btn-outline-primary btn-sm" href="#">Submit event</a>
-                          <!-- <a class="btn btn-outline-primary btn-sm dropdown-toggle" href="#">Submit event</a>
-                          <ul class="dropdown-menu" width="auto">
-                                <li><a class="dropdown-item" href="{{route('coi.exhibition', ['eventype' => 'expo'])}}">More</a></li>
-                                <li><a class="dropdown-item" href="#">Exhibit</a></li>
-                                <li><a class="dropdown-item" href="{{route('coievent.add', ['board' => 'add-your-event'])}}">Add Event</a></li>
-                                <li><a class="dropdown-item" href="{{route('coievent.add', ['board' => 'add-your-event'])}}">Add Event</a></li>        
-                              </ul> -->
-                        </span>
-                      </div>
-                </div>
+                <div class="container">
+                  <div class="list-unstyled pt-2 pb-0 px-0 pl-0">
+                        <div class="d-flex justify-content-between px-0 m-0 lh-1 ">
+                          <span class="fs-sm"> 2Concurrent<br><span class="fw-medium h5">Event</span></span>
+                          <a class="btn btn-outline-primary btn-sm" href="#">Submit event</a>
+                            <!-- <a class="btn btn-outline-primary btn-sm dropdown-toggle" href="#">Submit event</a>
+                            <ul class="dropdown-menu" width="auto">
+                                  <li><a class="dropdown-item" href="{{route('coi.exhibition', ['eventype' => 'expo'])}}">More</a></li>
+                                  <li><a class="dropdown-item" href="#">Exhibit</a></li>
+                                  <li><a class="dropdown-item" href="{{route('coievent.add', ['board' => 'add-your-event'])}}">Add Event</a></li>
+                                  <li><a class="dropdown-item" href="{{route('coievent.add', ['board' => 'add-your-event'])}}">Add Event</a></li>        
+                                </ul> -->
+                          </span>
+                        </div>
+                  </div>
 
-                <div class="row g-0 py-0 mx-n2 my-Slider4 mt-2"> 
-                  {{-- px-2 mb-1 --}}
-                  @foreach($relativeEvent as $eventoi)
-                    <div wire:ignore class="col-lg-2 col-md-4 col-sm-6 px-2 mb-1" href="{{route('event.details',['slug' => $eventoi->slug])}}">
-                      <div class="card product-card">
-                        <a class="card-img-top d-block overflow-hidden" href="{{route('event.details',['slug' => $eventoi->slug])}}">
-                        <img src="{{url('public/assets/image/exhibition/'.$eventoi->image)}}" alt=""> </a>
+                  <div class="row g-0 py-0 mx-n2 my-Slider4 mt-2"> 
+                    {{-- px-2 mb-1 --}}
+                    @foreach($relativeEvent as $eventoi)
+                      <div wire:ignore class="col-lg-2 col-md-4 col-sm-6 px-2 mb-1" href="{{route('event.details',['slug' => $eventoi->slug])}}">
+                        <div class="card product-card">
+                          <a class="card-img-top d-block overflow-hidden" href="{{route('event.details',['slug' => $eventoi->slug])}}">
+                          <img src="{{url('public/assets/image/exhibition/'.$eventoi->image)}}" alt=""> </a>
 
-                        <div class="card-body p-1">
-                          <div class="d-flex justify-content-between">
-                              <div class="product-price"><small>{{$eventoi -> edition}}  
-                                <i class="bi bi-shield-check" data-bs-toggle="tooltip" data-bs-placement="left" title="" data-bs-original-title="certified" aria-label="certified">
-                                    <span class="fs-xs">
-                                      @php
-                                          $to = strtotime($eventoi->startdate);
-                                          $from= strtotime($eventoi->enddate);
-                                      @endphp
-                                      
+                          <div class="card-body p-1">
+                            <div class="d-flex justify-content-between">
+                                <div class="product-price"><small>{{$eventoi -> edition}}  
+                                  <i class="bi bi-shield-check" data-bs-toggle="tooltip" data-bs-placement="left" title="" data-bs-original-title="certified" aria-label="certified">
+                                      <span class="fs-xs">
+                                        @php
+                                            $to = strtotime($eventoi->startdate);
+                                            $from= strtotime($eventoi->enddate);
+                                        @endphp
+                                        
 
-                                      @if ($current < $to && $current < $from)
-                                          Upcom
-                                        @elseif ($current == $to && $current < $from) 
-                                          First
-                                        @elseif ($current > $to && $current < $from) 
-                                          Ongoi
-                                        @elseif ($current > $to && $current == $from) 
-                                          Last 
-                                        @elseif ($current > $to && $current > $from)
-                                          Ended
-                                      @endif
-                                    </span>
-                                  <i class="bi bi-lightning-fill" data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="upcoming" aria-label="upcoming"></i></i></small>
-                                <div class="product-title fs-sm h3 mb-0">
-                                <a href="{{route('event.details',['slug' => $eventoi->slug])}}">{{ucwords(trans($eventoi -> eventname))}}
-                                  </a></div>
-                              </div>
-
-                              <div class="star-rating d-none d-sm-block"> 
-                                <small> <span class="badge bg-primary opacity-75" style="position: unset;"> Visitor</span> | <span class="badge bg-primary opacity-75" style="position: unset;"> Exhibit</span></small>       
-                                <div class=" align-center fs-sm py-1"> 
-                                  <small class="mx-0" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="Visitor" aria-label="Visitor"> + {{$eventoi -> auidence}} <i class="bi bi-people-fill"></i></small> 
-                                  <small class="mx-1" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="Exhibitor" aria-label="Exhibior">+ {{$eventoi -> exhibitors}}K <i class="bi bi-person-workspace"></i></small>
+                                        @if ($current < $to && $current < $from)
+                                            Upcom
+                                          @elseif ($current == $to && $current < $from) 
+                                            First
+                                          @elseif ($current > $to && $current < $from) 
+                                            Ongoi
+                                          @elseif ($current > $to && $current == $from) 
+                                            Last 
+                                          @elseif ($current > $to && $current > $from)
+                                            Ended
+                                        @endif
+                                      </span>
+                                    <i class="bi bi-lightning-fill" data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="upcoming" aria-label="upcoming"></i></i></small>
+                                  <div class="product-title fs-sm h3 mb-0">
+                                  <a href="{{route('event.details',['slug' => $eventoi->slug])}}">{{ucwords(trans($eventoi -> eventname))}}
+                                    </a></div>
                                 </div>
-                              </div>
+
+                                <div class="star-rating d-none d-sm-block"> 
+                                  <small> <span class="badge bg-primary opacity-75" style="position: unset;"> Visitor</span> | <span class="badge bg-primary opacity-75" style="position: unset;"> Exhibit</span></small>       
+                                  <div class=" align-center fs-sm py-1"> 
+                                    <small class="mx-0" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="Visitor" aria-label="Visitor"> + {{$eventoi -> auidence}} <i class="bi bi-people-fill"></i></small> 
+                                    <small class="mx-1" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="Exhibitor" aria-label="Exhibior">+ {{$eventoi -> exhibitors}}K <i class="bi bi-person-workspace"></i></small>
+                                  </div>
+                                </div>
+                            </div>
+                            <!--<small>World's best demanding business</small><br>-->
+                            <small class="text-bolder d-none d-sm-block"> <i class="bi bi-calendar3"></i>
+                              @if(Carbon\Carbon::parse ($eventoi->startdate)->format('M') != Carbon\Carbon::parse ($eventoi->enddate)->format('M'))
+                                {{Carbon\Carbon::parse ($eventoi->startdate)->format('D, d M')}} - {{Carbon\Carbon::parse ($eventoi->enddate)->format('D, d M Y ')}}
+                              @else
+                                {{Carbon\Carbon::parse ($eventoi->startdate)->format('D, d ')}} - {{Carbon\Carbon::parse ($eventoi->enddate)->format('D, d M Y')}}
+                              @endif 
+
+                            </small>
+                            <small  class="d-none d-sm-block"><i class="bi bi-geo-alt-fill fs-sm"></i>{{ucwords(trans($eventoi -> venue ?? ''))}}, <br> {{ucwords(trans($eventoi -> city ?? ''))}}</small>
+
+                            <small class="text-bolder d-lg-none"> <i class="bi bi-calendar3"></i>
+                              @if(Carbon\Carbon::parse ($eventoi->startdate)->format('M') != Carbon\Carbon::parse ($eventoi->enddate)->format('M'))
+                                {{Carbon\Carbon::parse ($eventoi->startdate)->format('d M')}} - {{Carbon\Carbon::parse ($eventoi->enddate)->format('d M, y')}}
+                              @else
+                                {{Carbon\Carbon::parse ($eventoi->startdate)->format('d ')}} - {{Carbon\Carbon::parse ($eventoi->enddate)->format('d M, y')}}
+                              @endif 
+                            </small><br>
+                            <small class="d-lg-none"><i class="bi bi-geo-alt-fill fs-sm"></i>{{ucwords(trans($eventoi -> city ?? ''))}}</small> 
+                            <!--ucfirst-->
                           </div>
-                          <!--<small>World's best demanding business</small><br>-->
-                          <small class="text-bolder d-none d-sm-block"> <i class="bi bi-calendar3"></i>
-                            @if(Carbon\Carbon::parse ($eventoi->startdate)->format('M') != Carbon\Carbon::parse ($eventoi->enddate)->format('M'))
-                              {{Carbon\Carbon::parse ($eventoi->startdate)->format('D, d M')}} - {{Carbon\Carbon::parse ($eventoi->enddate)->format('D, d M Y ')}}
-                            @else
-                              {{Carbon\Carbon::parse ($eventoi->startdate)->format('D, d ')}} - {{Carbon\Carbon::parse ($eventoi->enddate)->format('D, d M Y')}}
-                            @endif 
 
-                          </small>
-                          <small  class="d-none d-sm-block"><i class="bi bi-geo-alt-fill fs-sm"></i>{{ucwords(trans($eventoi -> venue ?? ''))}}, <br> {{ucwords(trans($eventoi -> city ?? ''))}}</small>
-
-                          <small class="text-bolder d-lg-none"> <i class="bi bi-calendar3"></i>
-                            @if(Carbon\Carbon::parse ($eventoi->startdate)->format('M') != Carbon\Carbon::parse ($eventoi->enddate)->format('M'))
-                              {{Carbon\Carbon::parse ($eventoi->startdate)->format('d M')}} - {{Carbon\Carbon::parse ($eventoi->enddate)->format('d M, y')}}
-                            @else
-                              {{Carbon\Carbon::parse ($eventoi->startdate)->format('d ')}} - {{Carbon\Carbon::parse ($eventoi->enddate)->format('d M, y')}}
-                            @endif 
-                          </small><br>
-                          <small class="d-lg-none"><i class="bi bi-geo-alt-fill fs-sm"></i>{{ucwords(trans($eventoi -> city ?? ''))}}</small> 
-                          <!--ucfirst-->
+                          
+                          
+                          <div class="card-body card-body-hidden">
+                            <div class="d-flex justify-content-between mb-2">
+                              <a class="btn btn-primary btn-sm d-block w-50 mx-1" type="button" href="#"><i class=" bi bi-brush fs-sm me-1"></i>Exhibit</a>
+                              <a class="btn btn-primary btn-sm d-block w-50 mx-1" type="button" href="#"><i class=" bi bi-cart fs-sm me-1"></i>Visit</a>
+                            </div>
+                          
+                            <div class="text-center">
+                              @guest<a class="nav-link-style fs-ms" href="#" data-bs-toggle="modal">
+                              <i class=" bi bi-eye align-middle me-1"></i>Contact</a>
+                              @endguest
+                            </div>
+                          </div>
+                        
                         </div>
-
-                        
-                        
-                        <div class="card-body card-body-hidden">
-                          <div class="d-flex justify-content-between mb-2">
-                            <a class="btn btn-primary btn-sm d-block w-50 mx-1" type="button" href="#"><i class=" bi bi-brush fs-sm me-1"></i>Exhibit</a>
-                            <a class="btn btn-primary btn-sm d-block w-50 mx-1" type="button" href="#"><i class=" bi bi-cart fs-sm me-1"></i>Visit</a>
-                          </div>
-                        
-                          <div class="text-center">
-                            @guest<a class="nav-link-style fs-ms" href="#" data-bs-toggle="modal">
-                            <i class=" bi bi-eye align-middle me-1"></i>Contact</a>
-                            @endguest
-                          </div>
-                        </div>
-                      
                       </div>
-                    </div>
-                  @endforeach
+                    @endforeach
+                  </div>
                 </div>
               @endif
                   
