ff6e367e2c65f68f54f617d503dce68d75890b3f
lara <laravel8coi@gmail.com>
2024-11-17 19:55:06 +0530

    17nov20240754

--- resources/views/livewire/exhibit-component.blade.php
+++ resources/views/livewire/exhibit-component.blade.php
@@ -12,7 +12,7 @@
 
               <div class="col-lg-8 col-md-10">
                 <h2 class="h3 mb-2">Exhibit with us.</h2>
-                <p class="fs-md">business Join the ranks of top industry leaders and connect with your target audience. <br> Share your details and let's make it happen </p>
+                <p class="fs-md">Join the ranks of top industry leaders and connect with your target audience. <br> Share your details and let's make it happen </p>
                 {{--<ol class="list-unstyled fs-md">
                   <li><span class="text-primary me-2">1.</span>Fill in your email address below.</li>
                   <li><span class="text-primary me-2">2.</span>We'll email you a temporary code.</li>
@@ -315,7 +315,7 @@
             </div>
           </div>
         @elseif($this->board == 'hostess')
-        <div class="container py-4 py-lg-5 my-4">
+          <div class="container py-4 py-lg-5 my-4">
             <div class="row justify-content-center">
 
 
@@ -359,6 +359,88 @@
 
             </div>
           </div>
+        @elseif($this->board == 'embassy')
+        <div class="container py-4 py-lg-5 my-4">
+            <div class="row justify-content-center">
+
+
+              <div class="col-lg-8 col-md-10">
+                <h2 class="h3 mb-2">Find Right place to meet.</h2>
+                <p class="fs-md">Join the ranks of top industry leaders and connect. <br> Share your details and let's make it happen </p>
+                {{--<ol class="list-unstyled fs-md">
+                  <li><span class="text-primary me-2">1.</span>Fill in your email address below.</li>
+                  <li><span class="text-primary me-2">2.</span>We'll email you a temporary code.</li>
+                  <li><span class="text-primary me-2">3.</span>Use the code to change your password on our secure website.</li>
+                </ol>--}}
+
+                
+                  <form  wire:submit.prevent="vipentryadd" >
+                    <div class="mb-1">
+                      <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                      <input class="form-control" type="name"  wire:model.lazy="name" required="" placeholder="Enter your name">
+                      @error('name')
+                      <div class="form-text text-primary">{{$message}}</div>
+                    @enderror
+                    </div>
+
+                    <div class="mb-1">
+                      <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                      <input class="form-control" type="name"  wire:model.lazy="designation" required="" placeholder="Your Designation">
+                      @error('name')
+                      <div class="form-text text-primary">{{$message}}</div>
+                    @enderror
+                    </div>
+
+                    <div class="mb-1">
+                      <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                      <input class="form-control" type="email"  wire:model.lazy="email" required="" placeholder="Enter your email address">
+                      @error('email')
+                      <div class="form-text text-primary">{{$message}}</div>
+                    @enderror
+                    </div>
+
+                    <div class="mb-1">
+                      <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                      <input class="form-control" type="email"  wire:model.lazy="email" required="" placeholder="Enter your email address">
+                      @error('email')
+                      <div class="form-text text-primary">{{$message}}</div>
+                    @enderror
+                    </div>
+ 
+                    <div class="mb-1">
+                      <!-- <label class="form-label" for="recover-email">Enter your email address</label> -->
+                      <input class="form-control" type="text"  wire:model.lazy="embassyname" required="" placeholder="Enter your embassy Name">
+                      @error('email')
+                      <div class="form-text text-primary">{{$message}}</div>
+                    @enderror
+                    </div>
+
+                    <div class="mb-1">
+                      <!-- <label class="form-label" for="recover-email">Enter your phone</label> -->
+                      <input class="form-control" type="number"  wire:model.lazy="phone" required="" placeholder="Enter your phone">
+                      @error('phone')
+                        <div class="form-text text-primary">{{$message}}</div>
+                      @enderror
+                    </div>
+
+                    <div class="mb-1">
+                      <label class="form-label" for="recover-email">upload your business Image</label> -->
+                      <input class="form-control" type="file"  wire:model.lazy="image" required="" placeholder="upload your ">
+                      @error('phone')
+                        <div class="form-text text-primary">{{$message}}</div>
+                      @enderror
+                    </div>
+
+
+
+                    <button class="btn btn-primary form-control" type="submit">Submit</button>
+                  </form>
+              </div>
+
+
+            </div>
+          </div>
+
         @endif
 
         @if($board == 'registration-great-exhibition-to-visit')
