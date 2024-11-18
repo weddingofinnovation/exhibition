aaee3d33bfafd9b970b4e22e06871663a5e6cf99
lara <laravel8coi@gmail.com>
2024-11-18 23:18:03 +0530

    18nov20241117

--- app/Http/Livewire/Admin/AdminQuestionComponent.php
+++ app/Http/Livewire/Admin/AdminQuestionComponent.php
@@ -17,13 +17,45 @@ class AdminQuestionComponent extends Component
     public $quest;
     public $eventid;
 
+    public $questions = [];
+    public $selectedQuestions = [];
+
     public function mount($eventid = null)
     {
         $this->eventid = $eventid;
+        $this->questions = Question::all();
+
+        if($this->eventid)
+        {
+          $this->selectedQuestions  = Question::where('event_id', $this->eventid)->pluck('question_id')->toArray();
+
+        }
         // $this->month = Carbon::today()->format("m");
         // $this->monthly = Carbon::today()->format("m");
         // $this->visited = '1';
         // $this->currentTab = session()->get('currentTab','tab1');
+    }
+
+    public function toggleQuestion($questionId)
+    {
+        if(!$this->eventid)
+        {
+            return;
+        }
+
+        if(in_array($questionId, $this->selectedQuestions))
+        {
+            Question::where('event_id', $this->eventid)->where('question_id' ,$questionId)->delete();
+            $this->selectedQuestions = array_diff($this->selectedQuestions, [$questionId]);
+        }
+        else
+        {
+            Question::create([
+                'event_id' => $this->eventid,
+                'question_id' => $questionId,
+            ]);
+            $this->selectedQuestions[] = $questionId;
+        }
     }
 
     public function questadd()
@@ -73,6 +105,6 @@ class AdminQuestionComponent extends Component
     public function render()
     {
         $result = Question::get();
-        return view('livewire.admin.admin-question-component',['result' => $result]);
+        return view('livewire.admin.admin-question-component',['result' => $result, 'selectionEnabled' => $this->eventid ? true : false,]);
     }
 }
--- resources/views/livewire/admin/admin-question-component.blade.php
+++ resources/views/livewire/admin/admin-question-component.blade.php
@@ -7,39 +7,79 @@
     </div>
 
     <div class="container my-3">
-        @foreach($result as $evento)
-        <div class="row text-center p-1 gx-0 mb-1  shadow-sm  border rounded border-1">
-            <div class="col  pr-0">
-                
-                  <div class="h4 fw-light mb-0"></div> 
-                  <div class="small text-muted text-capitalize"></div>
+        @if($selectionEnabled)
+            @foreach($result as $evento)
+                <div class="row text-center p-1 gx-0 mb-1  shadow-sm  border rounded border-1">
+                    <div class="col  pr-0" wire:click="toggleQuestion({{$evento->id}})">
+                        
+                        <div class="h4 fw-light mb-0"></div> 
+                        <div class="small text-muted text-capitalize"></div>
 
-                <div class="round-circle">{{$evento->id}}</div> 
-                {{--<a class="btn btn-primary btn-sm" href="{{$link->google()}}">Add to Calender</a>--}}
-            </div>
+                        @if(in_array($question->id, $selectionQuestions))
+                            <div class="round-circle">{{$evento->id}}</div> Add
+                        @endif
 
-            <div class="col-7  p-0">
-              <div class="fs-sm fw-normal text-start">
-                    <a class="text-dark" href="#">
-                    {{$evento->question}}</a>
-                
-              </div>
+                        {{--<a class="btn btn-primary btn-sm" href="{{$link->google()}}">Add to Calender</a>--}}
+                    </div>
 
-              <div class="text-muted fs-sm text-start">
-                  {{$evento->event_id}}
-              </div>  
-              <div class="text-muted fs-sm text-start"></div>
-            </div>
+                    <div class="col-7  p-0">
+                    <div class="fs-sm fw-normal text-start">
+                            <a class="text-dark" href="#">
+                            {{$evento->question}}</a>
+                        
+                    </div>
 
-            <div class="col-3  p-0">
-                <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="delete({{$evento->id}})"> <i class="bi bi-x"></i> </a>
+                    <div class="text-muted fs-sm text-start">
+                        {{$evento->event_id}}
+                    </div>  
+                    <div class="text-muted fs-sm text-start"></div>
+                    </div>
 
-                <!-- <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="admstatus({{$evento->id}},'1')"> <i class="bi bi-plus"></i>Act </a>
-                <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="admstatus({{$evento->id}},'0')"> <i class="bi bi-active"></i>DeA </a> -->
+                    <div class="col-3  p-0">
+                        <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="delete({{$evento->id}})"> <i class="bi bi-x"></i> </a>
 
-                <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="updated({{$evento->id}},'0')"> <i class="bi bi-pencil"></i> </a>
-            </div>
-        </div>
-        @endforeach
+                        <!-- <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="admstatus({{$evento->id}},'1')"> <i class="bi bi-plus"></i>Act </a>
+                        <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="admstatus({{$evento->id}},'0')"> <i class="bi bi-active"></i>DeA </a> -->
+
+                        <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="updated({{$evento->id}},'0')"> <i class="bi bi-pencil"></i> </a>
+                    </div>
+                </div>
+            @endforeach
+        @else
+            @foreach($result as $evento)
+                <div class="row text-center p-1 gx-0 mb-1  shadow-sm  border rounded border-1">
+                    <div class="col  pr-0">
+                        
+                        <div class="h4 fw-light mb-0"></div> 
+                        <div class="small text-muted text-capitalize"></div>
+
+                        <div class="round-circle">{{$evento->id}}</div> 
+                        {{--<a class="btn btn-primary btn-sm" href="{{$link->google()}}">Add to Calender</a>--}}
+                    </div>
+
+                    <div class="col-7  p-0">
+                    <div class="fs-sm fw-normal text-start">
+                            <a class="text-dark" href="#">
+                            {{$evento->question}}</a>
+                        
+                    </div>
+
+                    <div class="text-muted fs-sm text-start">
+                        {{$evento->event_id}}
+                    </div>  
+                    <div class="text-muted fs-sm text-start"></div>
+                    </div>
+
+                    <div class="col-3  p-0">
+                        <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="delete({{$evento->id}})"> <i class="bi bi-x"></i> </a>
+
+                        <!-- <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="admstatus({{$evento->id}},'1')"> <i class="bi bi-plus"></i>Act </a>
+                        <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="admstatus({{$evento->id}},'0')"> <i class="bi bi-active"></i>DeA </a> -->
+
+                        <a href="#" class="btn btn-sm btn-dark-outline" wire:click.prevent="updated({{$evento->id}},'0')"> <i class="bi bi-pencil"></i> </a>
+                    </div>
+                </div>
+            @endforeach
+        @endif
     </div>
 </main>
