b94bf4599260bda9dd1bda29c9c16f9ae7e25fb0
lara <laravel8coi@gmail.com>
2025-01-29 15:35:38 +0530

    29jan20250335

--- resources/views/livewire/exhibit-component.blade.php
+++ resources/views/livewire/exhibit-component.blade.php
@@ -483,11 +483,8 @@
                   
                   https://exhibition.org.in/exhibit/thankyou/712
                   
-                  Thank you for registering to attend <Strong>{{$findevent->eventname}}</Strong>, taking place from {{$findevent->startdate}} at {{$findevent->city}}
-
-                  We are excited to welcome you to {{$findevent->country}}'s flagship Category Of Event, where innovation and collaboration shape the future of the global -- ecosystem.
-
-                  Your badge will be sent to you 4 weeks prior to the event. In the meantime, please find some information on your visitor pass benefits and conference upgrade options.
+                  <p class="fs-sm">We are excited to welcome you to {{$findevent->country}}'s flagship Category Of Event, where innovation and collaboration shape the future of the global -- ecosystem.
+                  Your badge will be sent to you 4 weeks prior to the event. In the meantime, please find some information on your visitor pass benefits and conference upgrade options.</p>
 
                   <!-- <div class = "mt-3">
                       <strong>VISA Requirements for International attendees</strong> 
@@ -500,18 +497,15 @@
                   </div> -->
                   
 
-                  <div class="row justify-content-center text-center">{{QrCode::size(180)->generate(url('/birthday'))}}</div>
-                  {{-- <div class="row justify-content-center text-center">{{QrCode::size(180)->generate(url('/birthday'.detail->wabill))}}</div> --}}
+            {{-- <div class="row justify-content-center text-center">{{QrCode::size(180)->generate(url('/birthday'))}}</div> --}}
+            {{-- <div class="row justify-content-center text-center">{{QrCode::size(180)->generate(url('/birthday'.detail->wabill))}}</div> --}}
 
 
                   <div class = "mt-3">
-                    <strong>Travel Made easy to {{$findevent->eventname}}</strong>
-
+                    <strong>Travel Made Easy</strong>
                     <p>Our Official Travel Partner, to arrange all your travel and accommodation needs.</p>
-                    
-                    <p>Enjoy exclusive benefits like <strong>preferential hotal rates and seamless travel arrangements</strong> as you plan your visit to 
-                    <Strong>{{$findevent->eventname}}</Strong> in {{$findevent->city}}</p>
-                    
+                    <p class="fs-sm">Enjoy exclusive benefits like <a href="">preferential hotal rates and seamless travel arrangements</a> as you plan your visit to 
+                    <Strong>{{$findevent->eventname}}</Strong> in <strong>{{$findevent->city}}</strong></p>
                     <a href="" class="btn btn-md btn-primary">Add to Calender</a>
                   </div>
 
@@ -519,7 +513,7 @@
                     Disclaimer : Please note that cancellations are not permitted once the registration process is complete. Kindly reach out to us at 
                     <a href="">team@exhibition.org.in</a> for any registration related queries.
                   </div>
-                  
+
                 </div>
               </div>
             </div>
