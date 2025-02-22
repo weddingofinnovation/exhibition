71bd6b3e130b7112024dbb4d053280d900cb39fd
lara <laravel8coi@gmail.com>
2025-02-22 17:39:31 +0530

    22feb20250539

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -3933,10 +3933,15 @@
                         <div class="fw-bold text-center">{{$event->edition}} <i class="bi bi-patch-check-fill"></i></div>
                         <div class="fw-light fs-sm text-muted">Edition</div>
                       </div>
+
+                    @if($event->auidence == 0)
+                    @else
                       <div class="col-3 px-0 border-end">
                         <div class="fw-bold text-center ">{{number_format(($event->auidence / 1000) , 1). 'k'}} +</div>
                         <div class="fw-light fs-sm text-muted">Visitor</div>
                       </div>
+                    @endif
+                    
                       <div class="col-3 px-0 border-end">
                          <div class="fw-bold text-center">{{round($commentedRates->avg('rate') , 1)}}</div>
                          <div class="fw-light fs-xs text-muted">Rated for 3+</div>
