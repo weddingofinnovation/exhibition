37d5fef55b74345c3e341a4115fb6f69e1b79d8c
lara <laravel8coi@gmail.com>
2025-03-26 15:55:43 +0530

    26mar20250355

--- resources/views/livewire/event-component.blade.php
+++ resources/views/livewire/event-component.blade.php
@@ -19,19 +19,18 @@
                         <!-- <a class="btn btn-outline-primary mt-2" href="{{route('coievent.add',['board' => 'add-your-event'])}}">Search Right Place<i class="bi bi-chevron-right fs-ms ms-1"></i></a> -->
                       </div>
 
-                      <!-- <div class="px-4 pe-sm-0 ps-sm-5"><span class="badge bg-danger">Free</span>
+                  <!--<div class="px-4 pe-sm-0 ps-sm-5"><span class="badge bg-danger">Free</span>
                         {{--<h5 class="mt-4 mb-1 text-body fw-light">Free</h5>--}}
                          <h1 class="mb-1 display-1"> Great starts here.</h1>
                         <p class=" fw-light lh-1">Introducing Great Place To Business India's First Ever Event Publishing Platform</p>
                         <a class="btn btn-accent mt-4" href="{{route('coievent.add',['board' => 'add-your-event'])}}">Expand your Business <i class="bi bi-chevron-right fs-ms ms-1"></i></a>
+                      </div>-->
 
-                      </div> -->
                       <!-- <div class="px-4 pe-sm-0 ps-sm-5"><span class="badge bg-danger">Free</span>
                         {{--<h5 class="mt-4 mb-1 text-body fw-light">Free</h5>--}}
                         <h1 class="mb-1"> Add to calender <br> button</h1>
                         <p class=" fw-light lh-1">Share your events with the #1 add to calender button on the internet.</p>
                         <a class="btn btn-accent mt-4" href="{{route('coievent.add',['board' => 'add-your-event'])}}">Get your button <i class="bi bi-chevron-right fs-ms ms-1"></i></a>
-
                       </div> -->
 
                       {{--<div class="px-4 pe-sm-0 ps-sm-5"><span class="badge bg-danger">Get Certify</span>
