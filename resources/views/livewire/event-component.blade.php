fe5ba8ffd95187371404814405833b0721c599dd
lara <laravel8coi@gmail.com>
2025-03-27 14:51:50 +0530

    27mar20250251

--- resources/views/livewire/event-component.blade.php
+++ resources/views/livewire/event-component.blade.php
@@ -240,7 +240,7 @@
             </div>
         </div>
 
-        
+
 
         <style>
           * { box-sizing: border-box; }
@@ -510,19 +510,17 @@
         <div class="container mt-4">
             <!-- Featured Companies -->
             <div class="d-flex align-items-center mb-3">
-                <h5 class="me-3">Featured Companies</h5>
+                <h5 class="me-3">Popular Industries</h5>
                 <div class="featured-companies">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/HTC_logo.svg/2560px-HTC_logo.svg.png" alt="HTC" />
-                    <img src="https://1000logos.net/wp-content/uploads/2022/08/HDFC-Bank-Logo-500x281.png" alt="HDFC Bank" />
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="Apple" />
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/JP_Morgan_Chase_logo.svg" alt="JPMorgan Chase & Co." />
-                    <img src="https://www.valuelabs.com/wp-content/themes/valuelabs/assets/images/logo.svg" alt="ValueLabs" />
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" alt="" />
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg" alt="" />
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="" />
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg" alt="" />
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg" alt="" />
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Meta_Platforms_Inc._logo.svg" alt="" />
+                  @foreach ($getnamecategoryresult as $categ) 
+                    @php
+                      $findcountevent = DB::table('expos')->where('id', $categ->Category)->where('admstatus',1)->get();
+                    @endphp
+                    
+                    @foreach($findcountevent as $finderlo)
+                        <span class="category-badge" href="#" wire:click.prevent="insertEventToSess({{$finderlo->id}})">{{ucwords(trans($finderlo->tag))}}</span>
+                    @endforeach
+                  @endforeach
                 </div>
             </div>
 
