c4748d33b380c2aa1ef014e322ad156369850de6
lara <laravel8coi@gmail.com>
2025-04-01 10:34:11 +0530

    01apr20251034

--- resources/views/livewire/event-component.blade.php
+++ resources/views/livewire/event-component.blade.php
@@ -406,7 +406,7 @@
               </style>
 
             @php 
-              $venueoption = DB::table('locations')->whereNotNull('venue')->orderBy('created_at','desc')->get();
+              $venueoption = DB::table('locations')->whereNotNull('venue')->orderBy('created_at','desc')->limit(7)->get();
             @endphp
 
               @foreach($venueoption as $franchise)
