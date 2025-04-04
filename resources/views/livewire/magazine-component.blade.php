1abed891e62ba5f5d77b384722a6354add0c08e4
lara <laravel8coi@gmail.com>
2025-04-04 15:05:39 +0530

    04apr20250305

--- resources/views/livewire/magazine-component.blade.php
+++ resources/views/livewire/magazine-component.blade.php
@@ -59,9 +59,9 @@
     <section class="hero font-poppins">
         <div class="container">
             <h1>Great Exhibition To Exhibit</h1>
-            <p class="text-dark">For all Summit</p>
-            <p class="text-dark">Mumbai</p>
-            <p class="text-dark">India's largest and most sought-after workplace culture Summit</p>
+            <p class="text-dark fw-lg fs-md">For all Summit</p>
+            <p class="text-dark fw-bold">Mumbai</p>
+            <p class="text-dark fw- bold">India's largest and most sought-after workplace culture Summit</p>
             <!-- <p>Join us for an inspiring event filled with thought leaders and innovators.</p> -->
             <a href="#register" class="btn btn-primary btn-lg">Register for 2026 Summit</a>
         </div>
