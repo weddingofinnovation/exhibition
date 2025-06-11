d3683a46e7528453a371f7d804bcf48d9904b22b
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-06-11 19:55:28 +0530

    110620251955

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -4240,7 +4240,8 @@
 
                                 <div class="col text-end">
                                     <div class="stat-value d-flex flex-column align-items-end">
-                                        <h6 class="text-dark mb-0">5K+</h6>
+                                        <h6 class="fw-bold">Explore. Connect. Grow. Find Exhibitors Ready to Partner with You!</h6>
+                                        <small>Access verified exhibitor profiles, explore their offerings, and connect directly to discuss collaborations, partnerships, or bulk business deals — all in one place.</small>
                                         <ul class="avatar-group mb-0 d-flex justify-content-end">
                                             <a href="" class="btn btn-primary btn-sm">Download</a>
                                         </ul>
