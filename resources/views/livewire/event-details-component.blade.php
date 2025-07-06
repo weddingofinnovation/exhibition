c5717995251faffbeb0a0cf2d0de8323a0435dfc
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-07-06 14:09:37 +0530

    060720251409

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -3976,91 +3976,7 @@
                       </div>
                     </div>
 
-                    <!-- Right Side (Optional Block) -->
-                    <div class="col-12 col-md-6">
-                      <!-- Your second half goes here -->
-                       <!-- additive -->
-                            <style>
-                                .avatar-img {
-                                    width: 100%;
-                                    height: 100%;
-                                    -o-object-fit: cover;
-                                    object-fit: cover;
-                                }
-
-                                .avatar-group {
-                                    padding: 0;
-                                    display: -webkit-box;
-                                    display: -ms-flexbox;
-                                    display: flex;
-                                }
-
-                                .text-white {
-                                    --bs-text-opacity: 1;
-                                    --bs-white-rgb: 255, 255, 255;
-                                    color: rgba(var(--bs-white-rgb), var(--bs-text-opacity)) !important;
-                                }
-
-                                h6, .h6 {
-                                        font-size: 1.2rem;
-                                    }
-
-                                h6, .h6, h5, .h5, h4, .h4, h3, .h3, h2, .h2, h1, .h1 {
-                                    margin-top: 0;
-                                    margin-bottom: 0.5rem;
-                                    font-family: "Instrument Sans", sans-serif;
-                                    font-weight: 700;
-                                    line-height: 1.25;
-                                    color: var(--bs-heading-color);
-                                }
-
-                                .rounded-4 {
-                                    --bs-border-radius-xl: 0.6rem;
-                                    border-radius: var(--bs-border-radius-xl) !important;
-                                }
-                                .bg-dark {
-                                    --bs-bg-opacity: 1;
-                                    --bs-dark-rgb: 32, 33, 36;
-                                    background-color: rgba(var(--bs-dark-rgb), var(--bs-bg-opacity)) !important;
-                                }
-
-                                .avatar-group > li:not(:last-child) {
-                                      margin-right: -0.8rem;
-                                  }
-                                .avatar-group > li {
-                                    position: relative;
-                                }
-                                .avatar {
-                                    height: 3rem;
-                                    width: 3rem;
-                                    position: relative;
-                                    display: inline-block !important;
-                                }
-                                .avatar-xs {
-                                    height: 2.1875rem;
-                                    width: 2.1875rem;
-                                }
-                            </style>
-                        <!-- <div class="d-inline-block bg-dark rounded-4 position-absolute start-0 bottom-0 mb-md-4 ms-md-n5 p-3"> -->
-                            <div class="col-3 border-end">
-                            <div class="stat-value">
-                                <h6 class="text-dark mb-0 me-2">5K+</h6>
-                                <ul class="avatar-group mb-0 align-items-center">
-                                <li class="avatar avatar-xs">
-                                    <img class="avatar-img rounded-circle" src="https://www.exhibition.org.in/public/image/visi1.jpg" alt="avatar">
-                                </li>
-                                <li class="avatar avatar-xs">
-                                    <img class="avatar-img rounded-circle" src="https://www.exhibition.org.in/public/image/visi5.jpg" alt="avatar">
-                                </li>
-                                <li class="avatar avatar-xs">
-                                    <img class="avatar-img rounded-circle" src="https://www.exhibition.org.in/public/image/visi2.jpg" alt="avatar">
-                                </li>
-                                </ul>
-                            </div>
-                            <!-- <p class="text-dark stat-title">Total visitors per month</p> -->
-                            </div>
-                        <!-- </div> -->
-                    </div>
+                    
                   </div>
 
 
@@ -5868,6 +5784,7 @@
                                 <a class="btn btn-primary btn-sm mt-5" type="button" href="{{route('event.product',['slug' => $event->slug])}}"> Plan your Visit </a>
                             @endif
                         </span>
+                        <span>Call us at 999-185-6776</span>
                     </li>
 
                     <!-- <li><hr class="mt-md-2 mb-2"></li>
