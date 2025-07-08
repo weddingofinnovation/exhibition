17b3b8d18ca4139d7c82404440de47ae609431b1
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-07-08 14:42:23 +0530

    080720251442

--- app/Http/Livewire/Admin/AdminFranchiseAddComponent.php
+++ app/Http/Livewire/Admin/AdminFranchiseAddComponent.php
@@ -50,8 +50,7 @@ class AdminFranchiseAddComponent extends Component
 
       session()->flash('message','info has been deleted Successfully');
       $this->reset();
-      //return redirect()->back();
-      //return redirect()->route('admin.dashboard', ['board' => 'comment', ]);
+      dd('test is suucesgul');
     } 
 
     public function render()
