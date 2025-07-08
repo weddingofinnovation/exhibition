8bef00dd52b26079046a657d2bf587af7b7be027
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-07-08 13:13:10 +0530

    080720251313

--- resources/views/livewire/admin/admin-franchise-add-component.blade.php
+++ resources/views/livewire/admin/admin-franchise-add-component.blade.php
@@ -1,37 +1,7 @@
 <main>
-  <div class="container">
-    <form wire:submit.prevent="newcomment">
-        <div class="row g-1">
+  
 
-            <div class="col-sm-1">
-                <label class="form-label" for="seniority">Type</label>
-                <select class="form-control" type="text"   wire:model.lazy="response"  id="seniority"  placeholder="Provide short title of your request">
-                    <option selected>Choose</option>
-                    <option value="award">Award</option>
-                    <option value="conference">Conference, Forum, Seminar</option>
-                    <option value="expo">Exhibition</option>
-                    <option value="festival">Festival</option>
-                    <option value="network">Network</option>
-                    <option value="dealer">Dealer Distribution</option>
-                    <option value="embassy">Embassy</option>
-                </select>
-                @error('response') <div class="invalid-feedback">{{$message}}</div>@enderror
-            </div>
-
-            <div class="col-sm-4">
-                <label class="form-label" for="cf-name">Comment</label>
-                <input class="form-control" type="text" placeholder="Your comment"  wire:model.lazy="comment" required="">
-                @error('comment'){{$message}}@enderror
-            </div>
-            
-        </div>
-
-        <hr class="my-2">
-        <button class="btn btn-primary mt-2" type="submit">Submit</button>
-    </form>
-  </div>
-
-  <div class="bg-secondary d-lg-none mb-3">
+    <div class="bg-secondary d-lg-none mb-3">
         <div class="container">
             <div class="col-md-6 offset-md-3 d-flex justify-content-between ">
 
@@ -47,28 +17,9 @@
     <div class="container py-lg-5 my-lg-5">
         <form wire:submit.prevent="add">
             <!-- Title-->
-            <div class="align-items-center pb-5 d-none d-sm-block position-absolute">
-                <h3 class=" pt-2 me-2 text-center text-sm-start">How was the experience ?</h3>
-                <p class=" fs-sm  me-2 text-center text-sm-start">We hope your visit to this event was a experience! Please hit the appropriate rating below:</p>
-            </div>
-
-
-            <div class="pb-5 pb-sm-3"> 
-                <div class="form-label fw-bold">How would you rate your experience?</div> 
-                    <div class="d-flex justify-content-between">
-                    <div >0</div>
-                    <div >5</div>
-                    <div>10</div>
-                </div>
-
-                <input type="range" class="form-range" min="0" max="10" step="1" id="customRange3" wire:model.lazy="rate">
-                <small class="fs-xs  me-2 text-center text-sm-start text-muted">They help others decide what to visit next. </small>
-
-                @error('rate')
-                    <div class="form-text text-primary">{{$message}}</div>
-                @enderror
-            </div>
             
+
+
                 
             {{--@if(!is_null($rate))--}}
                 <div class="col-sm-2 col-md-12 pb-5 pb-sm-3">
