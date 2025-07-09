3dafd1c58f8645ff7628f3160e7bcf1baa6b60c1
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-07-09 21:46:53 +0530

    090720252146

--- resources/views/livewire/admin/admin-franchise-add-component.blade.php
+++ resources/views/livewire/admin/admin-franchise-add-component.blade.php
@@ -1,6 +1,6 @@
     @if($board == 'email') 
         <!-- email -->
-        <div class="container-fluid"> mank
+        <div class="container-fluid">
             <div class="row">
 
                 <!-- Search bar -->
