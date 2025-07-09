e365187335c62621f32ad9eae33b73e0b2d53a7a
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-07-09 15:12:19 +0530

    090720251512

--- resources/views/livewire/admin/admin-franchise-edit-component.blade.php
+++ resources/views/livewire/admin/admin-franchise-edit-component.blade.php
@@ -1,10 +1,12 @@
 <main>
-  @if(!empty($searchCat) && count($searchCat) > 0)
-    @foreach($searchCat as $franchise)
+<div class="container"> Mayank
+    @if(!empty($searchCat) && count($searchCat) > 0)
+        @foreach($searchCat as $franchise)
         {{-- your rendering logic --}} {{$franchise}}
-    @endforeach
-@else
-    <div class="container">Find Some Events</div>
-@endif
+        @endforeach
+    @else
+        <div class="container">Find Some Events</div>
+    @endif
+</div>
 
 </main>
