c927bb70f2f13eac4962718b1292856f2ff5b232
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-07-09 15:20:38 +0530

    090720251520

--- resources/views/livewire/admin/admin-franchise-edit-component.blade.php
+++ resources/views/livewire/admin/admin-franchise-edit-component.blade.php
@@ -1,12 +1,17 @@
 <main>
-<div class="container"> Mayanksing
-    @if(!empty($searchCat) && count($searchCat) > 0)
-        @foreach($searchCat as $franchise)
-        {{-- your rendering logic --}} {{$franchise}}
-        @endforeach
-    @else
-        <div class="container">Find Some Events</div>
-    @endif
-</div>
+  <div class="container"> Mayanksing
+
+
+  <input type="text" class="form-control" placeholder="search" wire:model.lazy="searchTerm">
+
+
+      @if(!empty($searchCat) && count($searchCat) > 0)
+          @foreach($this->searchCat as $franchise)
+          {{-- your rendering logic --}} {{$franchise}}
+          @endforeach
+      @else
+          <div class="container">Find Some Events</div>
+      @endif
+  </div>
 
 </main>
