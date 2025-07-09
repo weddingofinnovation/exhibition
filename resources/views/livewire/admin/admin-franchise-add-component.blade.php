e4f684104e7f2e0d424c152840468902a7737216
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-07-09 13:20:09 +0530

    090720251320

--- resources/views/livewire/admin/admin-franchise-add-component.blade.php
+++ resources/views/livewire/admin/admin-franchise-add-component.blade.php
@@ -1,4 +1,4 @@
-    @if($board == 'email') 
+     @if($board == 'email') 
         <!-- email -->
         <div class="container-fluid">
             <div class="row">
@@ -10,60 +10,8 @@
                         <button class="btn btn-primary">Search</button>
                     </div>
 
-                    <!-- Search Results -->
-                    @if(empty($searchTerm))
-                        <div class="text-center text-muted mb-3">Find Some Events</div>
-                        
-                        @else
-                        {{$searchCat}}
-
-                        <ul class="list-group">
-                            @foreach($searchCat as $franchise)
-                                <li class="list-group-item p-2">
-                                    <div class="row text-center gx-0">
-                                        <div class="col">
-                                            @if(Carbon\Carbon::parse($franchise->startdate)->format('M') != Carbon\Carbon::parse($franchise->enddate)->format('M'))
-                                                <div class="h4 fw-light mb-0">{{ Carbon\Carbon::parse($franchise->startdate)->format('d') }}</div>
-                                                <div class="small text-muted">{{ Carbon\Carbon::parse($franchise->startdate)->format('M y') }}</div>
-                                            @else
-                                                <div class="h4 fw-light mb-0">{{ Carbon\Carbon::parse($franchise->startdate)->format('d') }}</div>
-                                                <div class="small text-muted">{{ Carbon\Carbon::parse($franchise->startdate)->format('M y') }}</div>
-                                            @endif
-                                            <div class="round-circle">{{ $franchise->id }}</div>
-                                        </div>
-
-                                        <div class="col-7 p-0 text-start">
-                                            <div class="fs-md fw-normal">
-                                                <a class="text-dark" href="{{ route('adminevent.detail', ['slug' => $franchise->slug]) }}">
-                                                    {{ ucwords(Str::limit($franchise->eventname, 24)) }}
-                                                </a>
-                                            </div>
-                                            <div class="text-muted fs-sm">
-                                                @if(Carbon\Carbon::parse($franchise->startdate)->format('M') != Carbon\Carbon::parse($franchise->enddate)->format('M'))
-                                                    {{ Carbon\Carbon::parse($franchise->startdate)->format('D, d M') }} - {{ Carbon\Carbon::parse($franchise->enddate)->format('D, d M') }}
-                                                @else
-                                                    {{ Carbon\Carbon::parse($franchise->startdate)->format('D, d') }} - {{ Carbon\Carbon::parse($franchise->enddate)->format('D, d M') }}
-                                                @endif
-                                            </div>
-                                            <div class="text-muted fs-sm">{{ $franchise->venue }}, {{ $franchise->city }}</div>
-                                        </div>
-
-                                        <div class="col-3 p-0 d-flex flex-column justify-content-center">
-                                            <a class="text-danger mb-1" href="#"
-                                            onclick="confirm('Are you sure, You want to delete this Entity?') || event.stopImmediatePropagation()"
-                                            wire:click.prevent="eventdelete({{ $franchise->id }})">
-                                                <i class="bi bi-x me-2"></i> Delete
-                                            </a>
-                                            <a class="btn btn-sm btn-primary" href="#"
-                                            wire:click.prevent="updateInspectionStatus({{ $franchise->id }}, '1')">
-                                                Visit
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                            @endforeach
-                        </ul>
-                    @endif
+                    {{$searchCat}}
+                   
 
                 </div>
 
@@ -212,14 +160,10 @@
                           <input type="text" class="form-control" placeholder="search" wire:model.lazy="searchTerm">
 
                           {{$searchTerm}}
-                          
+
                           <div class="row mb-5 pb-2">
                             @if(is_null($searchTerm))
-
-                              <div class="container">
-                              Find Some Events
-                              </div>  
-
+                              <div class="container">Find Some Events</div>
                             @else
                               @foreach($searchCat as $franchise)
                                 <div class="container  ">
