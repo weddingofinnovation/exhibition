6cd908812aa971bb9ff8ecc47471c59a4cb7ce08
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-08-13 17:06:33 +0530

    130820251706

--- resources/views/livewire/business-design-strategy-component.blade.php
+++ resources/views/livewire/business-design-strategy-component.blade.php
@@ -170,7 +170,7 @@
         <!-- Live matches -->
         <div class="col-md-4">
           <div class="d-flex justify-content-between align-items-center mb-2">
-            <span class="fw-bold text-danger">LIVE matches</span>
+            <span class="fw-bold text-danger">LIVE matches</span>4
           </div>
 
           <div class="p-3 bg-dark rounded d-flex justify-content-between">
