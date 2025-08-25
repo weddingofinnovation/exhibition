978e3b5837d87635f961b1226e9bd1c19bb25f1e
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-08-25 21:28:17 +0530

    250820252128

--- resources/views/livewire/business-design-strategy-component.blade.php
+++ resources/views/livewire/business-design-strategy-component.blade.php
@@ -233,7 +233,7 @@
                         <img src="{{url('public/assets/image/exhibition/'.$franchise->image)}}" alt="Icon" class="me-2" style="width: 32px; height: 32px;">
                           <div>
                             <div class="small fw-bold">{{ucwords(trans(Str::limit($franchise->eventname, 24)))}}</div>
-                            <div class="text-muted small">PUBG, PUBG Players Tour, KRAFTON</div>
+                            <div class="text-muted small">{{ucwords(trans($eventoi -> city))}}</div>
                           </div>
                       </div>
                       
