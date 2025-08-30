614485a25ed83329794d6a1d99dc677aa31d5be4
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-08-30 09:41:59 +0530

    300820250941

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -3988,9 +3988,9 @@
                     <ul class="list-unstyled text-light">
                       <li class="d-flex">
                         <!-- <a class="btn btn-sm btn-primary mx-1" href="{{route('event.exhibit', ['board' => 'business'])}}">Plan to Visit</a> -->
-                        <!-- updated calender --> check
-                             {{$link->google()}}
-                        <a class ="btn btn-sm btn-primary mx-1" href="#">Add to Calender</a> 
+                        <!-- updated calender -->
+                            
+                        <a class ="btn btn-sm btn-primary mx-1" href="{{$link->google()}}">Add to Calender</a> 
                         <a class="btn btn-sm btn-light" href=""> <i class=" fs-md fw-dark bi bi-share"></i> Share it</a>
                         <a class="btn btn-sm btn-light" href=""> <i class="bi bi-bookmark-plus-fill"></i>Add To whislist</a>
                       </li>
