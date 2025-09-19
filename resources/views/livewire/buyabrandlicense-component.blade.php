1a4943a857b150dd850fc0eb1bdff1783abb9975
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-09-19 19:48:36 +0530

    190920251948

--- resources/views/livewire/buyabrandlicense-component.blade.php
+++ resources/views/livewire/buyabrandlicense-component.blade.php
@@ -34,6 +34,70 @@
     </section>
 
 
+     <!-- Order details-->
+            <h2 class="h6 pt-1 pb-3 mb-3 border-bottom">Review your order</h2>
+            <!-- Item-->
+            <div class="d-sm-flex justify-content-between my-4 pb-3 border-bottom">
+              <div class="d-sm-flex text-center text-sm-start"><a class="d-inline-block flex-shrink-0 mx-auto me-sm-4" href="shop-single-v1.html"><img src="img/shop/cart/01.jpg" width="160" alt="Product"></a>
+                <div class="pt-2">
+                  <h3 class="product-title fs-base mb-2"><a href="shop-single-v1.html">Women Colorblock Sneakers</a></h3>
+                  <div class="fs-sm"><span class="text-muted me-2">Size:</span>8.5</div>
+                  <div class="fs-sm"><span class="text-muted me-2">Color:</span>White &amp; Blue</div>
+                  <div class="fs-lg text-accent pt-2">$154.<small>00</small></div>
+                </div>
+              </div>
+              <div class="pt-2 pt-sm-0 ps-sm-3 mx-auto mx-sm-0 text-center text-sm-end" style="max-width: 9rem;">
+                <p class="mb-0"><span class="text-muted fs-sm">Quantity:</span><span>&nbsp;1</span></p>
+                <button class="btn btn-link px-0" type="button"><i class="ci-edit me-2"></i><span class="fs-sm">Edit</span></button>
+              </div>
+            </div>
+            <!-- Item-->
+            <div class="d-sm-flex justify-content-between my-4 pb-3 border-bottom">
+              <div class="d-sm-flex text-center text-sm-start"><a class="d-inline-block flex-shrink-0 mx-auto me-sm-4" href="shop-single-v1.html"><img src="img/shop/cart/02.jpg" width="160" alt="Product"></a>
+                <div class="pt-2">
+                  <h3 class="product-title fs-base mb-2"><a href="shop-single-v1.html">TH Jeans City Backpack</a></h3>
+                  <div class="fs-sm"><span class="text-muted me-2">Brand:</span>Tommy Hilfiger</div>
+                  <div class="fs-sm"><span class="text-muted me-2">Color:</span>Khaki</div>
+                  <div class="fs-lg text-accent pt-2">$79.<small>50</small></div>
+                </div>
+              </div>
+              <div class="pt-2 pt-sm-0 ps-sm-3 mx-auto mx-sm-0 text-center text-sm-end" style="max-width: 9rem;">
+                <p class="mb-0"><span class="text-muted fs-sm">Quantity:</span><span>&nbsp;1</span></p>
+                <button class="btn btn-link px-0" type="button"><i class="ci-edit me-2"></i><span class="fs-sm">Edit</span></button>
+              </div>
+            </div>
+            <!-- Item-->
+            <div class="d-sm-flex justify-content-between my-4 pb-3 border-bottom">
+              <div class="d-sm-flex text-center text-sm-start"><a class="d-inline-block flex-shrink-0 mx-auto me-sm-4" href="shop-single-v1.html"><img src="img/shop/cart/03.jpg" width="160" alt="Product"></a>
+                <div class="pt-2">
+                  <h3 class="product-title fs-base mb-2"><a href="shop-single-v1.html">3-Color Sun Stash Hat</a></h3>
+                  <div class="fs-sm"><span class="text-muted me-2">Brand:</span>The North Face</div>
+                  <div class="fs-sm"><span class="text-muted me-2">Color:</span>Pink / Beige / Dark blue</div>
+                  <div class="fs-lg text-accent pt-2">$22.<small>50</small></div>
+                </div>
+              </div>
+              <div class="pt-2 pt-sm-0 ps-sm-3 mx-auto mx-sm-0 text-center text-sm-end" style="max-width: 9rem;">
+                <p class="mb-0"><span class="text-muted fs-sm">Quantity:</span><span>&nbsp;1</span></p>
+                <button class="btn btn-link px-0" type="button"><i class="ci-edit me-2"></i><span class="fs-sm">Edit</span></button>
+              </div>
+            </div>
+            <!-- Item-->
+            <div class="d-sm-flex justify-content-between my-4 pb-3 border-bottom">
+              <div class="d-sm-flex text-center text-sm-start"><a class="d-inline-block flex-shrink-0 mx-auto me-sm-4" href="shop-single-v1.html"><img src="img/shop/cart/04.jpg" width="160" alt="Product"></a>
+                <div class="pt-2">
+                  <h3 class="product-title fs-base mb-2"><a href="shop-single-v1.html">Cotton Polo Regular Fit</a></h3>
+                  <div class="fs-sm"><span class="text-muted me-2">Size:</span>42</div>
+                  <div class="fs-sm"><span class="text-muted me-2">Color:</span>Light blue</div>
+                  <div class="fs-lg text-accent pt-2">$9.<small>00</small></div>
+                </div>
+              </div>
+              <div class="pt-2 pt-sm-0 ps-sm-3 mx-auto mx-sm-0 text-center text-sm-end" style="max-width: 9rem;">
+                <p class="mb-0"><span class="text-muted fs-sm">Quantity:</span><span>&nbsp;1</span></p>
+                <button class="btn btn-link px-0" type="button"><i class="ci-edit me-2"></i><span class="fs-sm">Edit</span></button>
+              </div>
+            </div>
+
+
       <!-- Bootstrap Icons CDN -->
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
