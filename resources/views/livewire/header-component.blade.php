f72773671248cf568f3cd5e72632b92c873acdd5
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-09-23 17:21:42 +0530

    230920251721

--- resources/views/livewire/header-component.blade.php
+++ resources/views/livewire/header-component.blade.php
@@ -75,7 +75,7 @@
     @else
       <header class="shadow-sm ">
         
-        <div class="topbar topbar-dark bg-dark d-none d-md-block" style="padding: 6px; font-size: 12px;">
+        <div class="topbar topbar-dark bg-dark d-none d-md-block" style="padding: 3px; font-size: 12px; color:#fff;">
           <div class="container">
             <div>
               <div class="topbar-text dropdown disable-autohide">
