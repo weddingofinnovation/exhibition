be771f44b63f685f01518fce4ccf4edb0185c5ee
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-09-30 16:21:41 +0530

    300920251621

--- resources/views/livewire/exhibition-category-component.blade.php
+++ resources/views/livewire/exhibition-category-component.blade.php
@@ -647,7 +647,7 @@
                             <!-- Meet for & button -->
                               <div class="border-top pt-2 mt-2">
                                   <small class="text-muted me-2">Meet for:</small>
-                                  <div class="d-flex justify-content-between align-items-center mt-2">
+                                  <div class="d-flex justify-content-between align-items-center">
                                       <div class="d-flex align-items-center gap-2">
                                           <img src="company1.png" alt="" class="rounded-circle" width="30" height="30">
                                           <img src="company2.png" alt="" class="rounded-circle" width="30" height="30">
