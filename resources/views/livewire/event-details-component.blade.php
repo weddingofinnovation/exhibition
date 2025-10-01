6ec46d5fcf927d2433293001a3a2f5a48b31ed0f
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-10-01 11:01:21 +0530

    011020251101

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -5766,12 +5766,12 @@
 
                     <div class="cl">
                         <div class="d-flex align-items-center justify-content-center mb-1">
-                                <div class="text-center me-3">
+                                <div class="text-center me-0">
                                     <h2 class="mb-0 fw-bold" style="font-size: 2.2rem;">{{$event->edition}}<sup style="font-size: 1rem;">a</sup></h2>
                                     <small class="text-uppercase">edition</small>
                                 </div>
 
-                                <div class="px-3 py-2 text-uppercase fw-bold me-3">{{$event->eventname}}<br> in Fiera</div>
+                                <div class="px-3 py-2 text-uppercase fw-bold fs-lg me-3">{{$event->eventname}}<br> in Fiera</div>
 
                                 <div class="h1 fw-bold">
                                     @if(Carbon::parse($event->startdate)->format('M') != Carbon::parse($event->enddate)->format('M'))
