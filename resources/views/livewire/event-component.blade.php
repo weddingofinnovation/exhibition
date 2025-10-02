c1f74c648ef5d2789823f0431afc8ce663d987a8
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-10-02 18:27:16 +0530

    021020251827

--- resources/views/livewire/event-component.blade.php
+++ resources/views/livewire/event-component.blade.php
@@ -277,18 +277,27 @@
         <div class="container-fluid featured-companies-bar d-none d-md-block">
             <div class="d-flex align-items-center">
                 <!-- <h5 class="me-2 mb-0">Featured Companies</h5> -->
-                <h5 class="me-2 mb-0">Featured by:</h5>
-                  <div class="scroll-container">
-                      @php 
-                        $photos = DB::table('brands')->whereNotNull('brand_logo')->paginate(50);
-                      @endphp
-                      <div class="scroll-content">
-                          @foreach($photos as $image)
-                            <img src="{{url('public/assets/image/exhibition/'.$image->brand_logo)}}" alt="Company Logo">
-                          @endforeach
-                      </div>
-                  </div>
-                <a class="btn btn-sm btn-primary me-2 mb-0" href="">Become our Client</a>
+                 <div class="bg-dark" style="width:325px;">
+                    <div class="">India's Biggest Exhibitor Database</div>
+                    <h5 class="me-2 mb-0">Find Exhibitor</h5>
+                 </div>
+                
+
+                <div class="scroll-container">
+                    @php 
+                      $photos = DB::table('brands')->whereNotNull('brand_logo')->paginate(50);
+                    @endphp
+                    <div class="scroll-content">
+                        @foreach($photos as $image)
+                          <img src="{{url('public/assets/image/exhibition/'.$image->brand_logo)}}" alt="Company Logo">
+                        @endforeach
+                    </div>
+                </div>
+                
+                <div class="" style="width:325px;"> 
+                  <h2>List your upcoming brand presence, Get free pre-meet.</h2>  
+                <a class="btn btn-sm btn-primary me-2 mb-0" href="">Become our Client</a></div>
+                
             </div>
         </div>
 
@@ -347,7 +356,7 @@
             <!-- Popular Companies -->
             <div class="d-flex align-items-center mb-3">
                 <h5 class="me-3">Popular Industries</h5>
-                <div class="featured-companies ">
+                <div class="featured-companies overflow-auto">
                   @foreach ($getnamecategoryresult as $categ) 
                     @php
                       $findcountevent = DB::table('expos')->where('id', $categ->Category)->where('admstatus',1)->get();
