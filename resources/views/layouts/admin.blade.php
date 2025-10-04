59c9135d013cc7c2d6dfbfc5a42dad8fd9d5ca22
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-10-04 20:37:24 +0530

    041020252037

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -58,7 +58,7 @@
     </main>
 
     testadmin
-      <!--<div class="page-title-overlap bg-dark pt-4">
+      <div class="page-title-overlap bg-dark pt-4">
         <div class="container d-lg-flex justify-content-between py-2 py-lg-3">
           <div class="order-lg-2 mb-3 mb-lg-0 pt-lg-2">
             <nav aria-label="breadcrumb">
@@ -74,7 +74,7 @@
             <h1 class="h3 text-light mb-0">@yield('page_name')</h1>
           </div>
         </div>
-      </div>-->
+      </div>
 
 
       <div class=" pb-5 mb-2 mb-md-4">{{$slot}}</div>
