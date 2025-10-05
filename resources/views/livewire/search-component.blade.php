399fca59fde7e2d609626753d371ef84438d38e5
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-10-05 10:48:04 +0530

    051020251047

--- resources/views/livewire/search-component.blade.php
+++ resources/views/livewire/search-component.blade.php
@@ -206,8 +206,8 @@
                       <!-- <a class="card-img-top d-block overflow-hidden" href="{{route('event.details',['slug' => $franchise->slug])}}">
                           <img src="{{url('public/assets/image/exhibition/'.$franchise->image)}}" alt="{{Str::limit($franchise->eventname, 24)}}"></a> -->
                     
-                            <a class="card-img-top d-block overflow-hidden" href="{{route('event.details',['slug' => $evet->slug])}}">
-                              <img src="{{url('public/assets/image/exhibition/'.$evet->image)}}" 
+                            <a class="card-img-top d-block overflow-hidden" href="{{route('event.details',['slug' => $franchise->slug])}}">
+                              <img src="{{url('public/assets/image/exhibition/'.$franchise->image)}}" 
                                   alt="{{Str::limit($evet->eventname, 24)}}" 
                                   class="img-fluid rounded" 
                                   style="height: 80px; width: auto; object-fit: contain;">
