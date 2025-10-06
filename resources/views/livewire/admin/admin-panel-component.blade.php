58fe3b02a9b46a9cba901c629f02311605b8f350
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-10-06 18:11:30 +0530

    061020251811

--- resources/views/livewire/admin/admin-panel-component.blade.php
+++ resources/views/livewire/admin/admin-panel-component.blade.php
@@ -65,7 +65,7 @@
                     <div class="d-flex justify-content-between align-items-center mb-2">
                       
                      
-                        <h4 class="fw-bold mb-2">Leads<small>{{$businessOrder->count()}}</small></h4>
+                        <h4 class="fw-bold mb-2">Leads<small>count</small></h4>
                           <div class="d-flex justify-content-between align-items-center">
                             <input type="search" class="form-control w-auto" placeholder="Search...">
                             <button class="btn btn-primary ml-4">
