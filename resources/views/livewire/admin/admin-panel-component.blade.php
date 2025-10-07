6f363d4d04ba884a334e4e2bdd95d9e86e760552
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-10-07 09:58:23 +0530

    071020250958

--- resources/views/livewire/admin/admin-panel-component.blade.php
+++ resources/views/livewire/admin/admin-panel-component.blade.php
@@ -228,10 +228,7 @@
                                         <h5 class="mb-1 fs-xs">
                                           <a class="nav-link-style stretched-link" href="#">1</a>
                                         </h5>
-                                            <!-- @php 
-                                                $Countstalldesignedonfloor = DB::table('stalls')->where('floorplan_id', $floor->id)->get();
-                                                $stallCount = $Countstalldesignedonfloor->count();
-                                            @endphp -->
+                                            
 
                                         <span class="fs-xs text-muted">Stalls</span>
                                      </div>
