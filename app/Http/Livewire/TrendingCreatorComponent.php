0eaab952e92c37449c943356ec4915c31a19faba
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-10-25 14:30:37 +0530

    251020251430

--- app/Http/Livewire/TrendingCreatorComponent.php
+++ app/Http/Livewire/TrendingCreatorComponent.php
@@ -8,24 +8,24 @@ use Livewire\Component;
 
 class TrendingCreatorComponent extends Component
 {
-    
-    public function insertEventToSess($id)
-    {
-      $event = Hostess::where('id', $id)->first();
-      $poostKey = 'HostessID';
-      $eid = $event->id;
-      Session()->put ($poostKey , $eid);
-      return redirect()->route('event.exhibit');
-    } admin
+
+  public function insertEventToSess($id)
+  {
+    $event = Hostess::where('id', $id)->first();
+    $poostKey = 'HostessID';
+    $eid = $event->id;
+    Session()->put($poostKey, $eid);
+    return redirect()->route('event.exhibit');
+  }
 
 
-    public function render()
-    {
-        $speaker = Speaker::where('admstatus','1')->where('status','1')->where('entity','speaker')->get();
-        $network = Speaker::where('admstatus','1')->where('status','1')->where('entity','network')->get();
-        $social = Speaker::where('admstatus','1')->where('status','1')->where('entity','social')->get();
+  public function render()
+  {
+    $speaker = Speaker::where('admstatus', '1')->where('status', '1')->where('entity', 'speaker')->get();
+    $network = Speaker::where('admstatus', '1')->where('status', '1')->where('entity', 'network')->get();
+    $social = Speaker::where('admstatus', '1')->where('status', '1')->where('entity', 'social')->get();
 
-        $hostess = Hostess::get();
-        return view('livewire.trending-creator-component',['speaker'=> $speaker,'network'=> $network,'social'=> $social, 'hostess' => $hostess]);
-    }
+    $hostess = Hostess::get();
+    return view('livewire.trending-creator-component', ['speaker' => $speaker, 'network' => $network, 'social' => $social, 'hostess' => $hostess]);
+  }
 }
