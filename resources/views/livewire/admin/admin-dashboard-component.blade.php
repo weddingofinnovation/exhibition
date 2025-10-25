1cd46f0bdc143bfa8a3cac66e9e7898be69ca5bc
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-10-25 16:27:20 +0530

    251020251627

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -748,29 +748,6 @@
           $network = DB::table('speakers')->where('admstatus', '1')->where('status', '1')->get();
           @endphp
 
-
-          <!--<h3 class="widget-title fw-bolder">Network</h3>-->
-          @foreach ($network as $franchise)
-          <div class="col-md-4 col-sm-6 mb-2 py-1">
-            <div class="d-flex align-items-center justify-content-between w-100 mb-2">
-              <div class="d-flex align-items-center position-relative">
-
-                <img class="rounded-circle ms-2" src="{{url('public/speaker/'.$franchise->image)}}" width="17%" alt="Avatar">
-                <div class="ms-2">
-                  <h4 class="mb-1 fs-base text-body">
-                    <a class="nav-link-style stretched-link" href="{{route('blog.author', ['slug' => $franchise->slug] )}}">{{$franchise->name}}</a>
-                  </h4>
-                  <h5 class="mb-1 fs-xs">
-                    <a class="nav-link-style stretched-link" href="#">{{$franchise->website}} {{$franchise->organisation}}</a>
-                  </h5>
-                  <!--<span class="fs-xs text-muted">730 followers</span>-->
-                </div>
-              </div>
-              <button class="btn btn-sm btn-outline-secondary ms-2">Follow</button>
-            </div>
-          </div>
-          @endforeach
-
           <div class="row">
             @foreach ($network as $franchise)
             <div class="col-md-4 col-sm-6 mb-3 py-2">
