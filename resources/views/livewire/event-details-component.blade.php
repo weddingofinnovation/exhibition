defeb731dcf29f37509241f01a8c57630739059f
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-11-10 09:12:04 +0530

    101120250912

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -4599,7 +4599,7 @@
                 <section class="container mb-5">
                     <div class="list-unstyled pt-2 pb-0 px-0 pl-0">
                         <div class="d-flex justify-content-between px-0 m-0 lh-1 ">
-                            <span class="fs-sm">Upcoming Event<br><span class="fw-medium h5">You may be interested in</span></span>
+                            <span class="fs-sm fw-bold">Upcoming Event <span class="fw-light fs-sm mt-1">You may be interested in</span></span>
                             <a class="btn btn-outline-primary btn-sm" href="#">Submit event</a>
                             <!-- <a class="btn btn-outline-primary btn-sm dropdown-toggle" href="#">Submit event</a>
                             <ul class="dropdown-menu" width="auto">
