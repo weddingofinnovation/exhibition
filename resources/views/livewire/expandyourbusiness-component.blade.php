0896a7a72db8647a8d262c65f2b4242a13996969
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-11-11 14:40:40 +0530

    111120251440

--- resources/views/livewire/expandyourbusiness-component.blade.php
+++ resources/views/livewire/expandyourbusiness-component.blade.php
@@ -163,12 +163,12 @@
       @endforeach
 
   @elseif ($board == 'exhibitor')
-    {{$this->event_id}}
+    
 
     @php 
-       $exhibitors = DB::table('brands')->get()
+       $exhibitors = DB::table('brands')->where('event_id', $this->event_id)->get()
     @endphp
-
+{{$this->event_id}} {{$exhibitors->count()}}
     @foreach($exhibitors as $exhibitor)
           <div class="exhibitor-item border-bottom py-3">
             <div class="row align-items-center g-2">
