b120c055e4985c5a0cb338a561d5ba680e7ff4cd
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-11-30 14:02:09 +0530

    301120251402

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,63 +1,113 @@
 <!DOCTYPE html>
-<html lang="en"><head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<style type="text/css" class="drift-base-styles">.drift-bounding-box,.drift-zoom-pane{position:absolute;pointer-events:none}@keyframes noop{0%{zoom:1}}@-webkit-keyframes noop{0%{zoom:1}}.drift-zoom-pane.drift-open{display:block}.drift-zoom-pane.drift-closing,.drift-zoom-pane.drift-opening{animation:noop 1ms;-webkit-animation:noop 1ms}.drift-zoom-pane{overflow:hidden;width:100%;height:100%;top:0;left:0}.drift-zoom-pane-loader{display:none}.drift-zoom-pane img{position:absolute;display:block;max-width:none;max-height:none}</style>
-    
-<title>@yield('page_title') | The Exhibition Network</title>
-    <!-- SEO Meta Tags <link rel="canonical" href="{{url()->current()}}"/>-->
-    <meta name="description" content="@yield('page_description')">
-    <meta name="keyword" content="@yield('page_keyword')">
-    <meta name="author" content="The Exhibtion Network">    
-    
-    <!-- Viewport-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Favicon and Touch Icons-->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css">
-  
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/image/favicons/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/image/favicons/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/image/favicons/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{asset('/image/favicons/site.webmanifest')}}">
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <style type="text/css" class="drift-base-styles">
+    .drift-bounding-box,
+    .drift-zoom-pane {
+      position: absolute;
+      pointer-events: none
+    }
+
+    @keyframes noop {
+      0% {
+        zoom: 1
+      }
+    }
+
+    @-webkit-keyframes noop {
+      0% {
+        zoom: 1
+      }
+    }
+
+    .drift-zoom-pane.drift-open {
+      display: block
+    }
+
+    .drift-zoom-pane.drift-closing,
+    .drift-zoom-pane.drift-opening {
+      animation: noop 1ms;
+      -webkit-animation: noop 1ms
+    }
+
+    .drift-zoom-pane {
+      overflow: hidden;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0
+    }
+
+    .drift-zoom-pane-loader {
+      display: none
+    }
+
+    .drift-zoom-pane img {
+      position: absolute;
+      display: block;
+      max-width: none;
+      max-height: none
+    }
+  </style>
+
+  <title>@yield('page_title') | The Exhibition Network</title>
+  <!-- SEO Meta Tags <link rel="canonical" href="{{url()->current()}}"/>-->
+  <meta name="description" content="@yield('page_description')">
+  <meta name="keyword" content="@yield('page_keyword')">
+  <meta name="author" content="The Exhibtion Network">
+
+  <!-- Viewport-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Favicon and Touch Icons-->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/image/favicons/apple-touch-icon.png')}}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/image/favicons/favicon-32x32.png')}}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/image/favicons/favicon-16x16.png')}}">
+  <link rel="manifest" href="{{asset('/image/favicons/site.webmanifest')}}">
 
 
-    <link rel="mask-icon" color="#fe6a6a" href="https://cartzilla.createx.studio/safari-pinned-tab.svg">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css">
+  <link rel="mask-icon" color="#fe6a6a" href="https://cartzilla.createx.studio/safari-pinned-tab.svg">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css">
 
 
-   
-<!-- NOTE: prior to v2.2.1 tiny-slider.js need to be in <body> -->
+
+  <!-- NOTE: prior to v2.2.1 tiny-slider.js need to be in <body> -->
 
 
-    <!-- Vendor Styles including: Font Icons, Plugins, etc.-->
-    <link rel="stylesheet" media="screen" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" media="screen" href="{{asset('css/simplebar.min.css')}}">
-    <link rel="stylesheet" media="screen" href="{{asset('css/tiny-slider.css')}}">
-    <link rel="stylesheet" media="screen" href="{{asset('css/drift-basic.min.css')}}">
-    <link rel="stylesheet" media="screen" href="{{asset('css/lightgallery.min.css')}}">
-    <link rel="stylesheet" media="screen" href="{{asset('css/theme.min.css')}}">
-   
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
- 
-    @livewireStyles
-  </head>
- 
-  <!-- Body-->
-  <body class="handheld-toolbar-enabled">
-    <!-- Sign in / sign up modal-->
-     <main class="page-wrapper" style="flex: 0 0 auto;">
-      
-     @livewire('seller-header-component')
-     
-    </main>
-    
-      <!--<div class="page-title-overlap bg-dark pt-4">
+  <!-- Vendor Styles including: Font Icons, Plugins, etc.-->
+  <link rel="stylesheet" media="screen" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" media="screen" href="{{asset('css/simplebar.min.css')}}">
+  <link rel="stylesheet" media="screen" href="{{asset('css/tiny-slider.css')}}">
+  <link rel="stylesheet" media="screen" href="{{asset('css/drift-basic.min.css')}}">
+  <link rel="stylesheet" media="screen" href="{{asset('css/lightgallery.min.css')}}">
+  <link rel="stylesheet" media="screen" href="{{asset('css/theme.min.css')}}">
+
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+  @livewireStyles
+</head>
+
+<!-- Body-->
+
+<body class="handheld-toolbar-enabled" style="padding-top: 80px;">
+  <!-- Sign in / sign up modal-->
+  <main class="page-wrapper" style="flex: 0 0 auto;">
+
+    @livewire('seller-header-component')
+
+  </main>
+
+  <!--<div class="page-title-overlap bg-dark pt-4">
         <div class="container d-lg-flex justify-content-between py-2 py-lg-3">
           <div class="order-lg-2 mb-3 mb-lg-0 pt-lg-2">
             <nav aria-label="breadcrumb">
@@ -76,34 +126,35 @@
       </div>-->
 
 
-      <div class=" pb-5 mb-2 mb-md-4">{{$slot}}</div>
+  <div class=" pb-5 mb-2 mb-md-4">{{$slot}}</div>
 
-   
 
-    <!-- Footer-->
-   
-    <!-- Vendor scrits: js libraries and plugins-->
-    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('js/simplebar.min.js')}}"></script>
-    <script src="{{asset('js/tiny-slider.js')}}"></script>
-    <script src="{{asset('js/app.js')}}"></script>
-    <script src="{{asset('js/smooth-scroll.polyfills.min.js')}}"></script>
-    <script src="{{asset('js/Drift.min.js')}}"></script>
-    <script src="{{asset('js/lightgallery.min.js')}}"></script>
-    <script src="{{asset('js/lg-video.min.js')}}"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js"></script>
 
-    <script type="text/javascript" src="https://unpkg.com/konva@9/konva.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/konva@8/konva.min.js"></script>
-    
+  <!-- Footer-->
 
-    <!-- Main theme script-->
-    <script src="{{asset('js/theme.min.js')}}"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha512-+NqPlbbtM1QqiK8ZAo4Yrj2c4lNQoGv8P79DPtKzj++l5jnN39rHA/xsqn8zE9l0uSoxaCdrOgFs6yjyfbBxSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-   
-    @livewireScripts
-   @stack('scripts')
+  <!-- Vendor scrits: js libraries and plugins-->
+  <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('js/simplebar.min.js')}}"></script>
+  <script src="{{asset('js/tiny-slider.js')}}"></script>
+  <script src="{{asset('js/app.js')}}"></script>
+  <script src="{{asset('js/smooth-scroll.polyfills.min.js')}}"></script>
+  <script src="{{asset('js/Drift.min.js')}}"></script>
+  <script src="{{asset('js/lightgallery.min.js')}}"></script>
+  <script src="{{asset('js/lg-video.min.js')}}"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js"></script>
+
+  <script type="text/javascript" src="https://unpkg.com/konva@9/konva.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/konva@8/konva.min.js"></script>
+
+
+  <!-- Main theme script-->
+  <script src="{{asset('js/theme.min.js')}}"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha512-+NqPlbbtM1QqiK8ZAo4Yrj2c4lNQoGv8P79DPtKzj++l5jnN39rHA/xsqn8zE9l0uSoxaCdrOgFs6yjyfbBxSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+  @livewireScripts
+  @stack('scripts')
 
 </body>
+
 </html>
