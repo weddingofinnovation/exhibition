f35281c17e822ba4945063f5d1917afae07ca473
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-12-02 12:14:44 +0530

    021220251214

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -4196,6 +4196,78 @@
                     <ul class="navbar-nav mb-2 mb-lg-0">
 
                         <li class="nav-item dropdown position-static">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Trade Fair</a>
+                            <div class="dropdown-menu mega-menu p-4 border-top-0">
+                                <div class="row">
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">ISM </h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Date and opening hours</a></li>
+                                            <li><a class="dropdown-item" href="#">Facts and Figures</a></li>
+                                            <li><a class="dropdown-item" href="#">Segment</a></li>
+                                            <li><a class="dropdown-item" href="#">Good Reasons</a></li>
+                                            <li><a class="dropdown-item" href="#">ISM app</a></li>
+                                            <li><a class="dropdown-item" href="#">Sponsorship cologne zoo</a></li>
+                                            <li><a class="dropdown-item" href="#">ISM Worldwide</a></li>
+                                            <li><a class="dropdown-item" href="#">Organisers & Partners</a></li>
+                                            <li><a class="dropdown-item" href="#">Review</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Experience ISM</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Halal Showcase</a></li>
+                                            <li><a class="dropdown-item" href="#">Trend Snacks</a></li>
+                                            <li><a class="dropdown-item" href="#">Specialist retail area</a></li>
+                                            <li><a class="dropdown-item" href="#">Scale-up Area</a></li>
+                                            <li><a class="dropdown-item" href="#">Start-up Area</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">ISM Ingredients</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Exhibitor application</a></li>
+                                            <li><a class="dropdown-item" href="#">Stand cost Calculation</a></li>
+                                            <li><a class="dropdown-item" href="#">Sponsoring Solutions ISM Ingredients</a></li>
+                                            <li><a class="dropdown-item" href="">Facts and figures</a></li>
+                                            <li><a class="dropdown-item" href="">Welcome Speech press breakfast</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Tickets</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Buy Tickets</a></li>
+                                            <li><a class="dropdown-item" href="#">Redeem Ticket Code</a></li>
+                                            <li><a class="dropdown-item" href="#">VIP-Buyer</a></li>
+                                            <li><a class="dropdown-item" href="#">Trade Visitors Definition</a></li>
+                                            <li><a class="dropdown-item" href="">Delegations</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Travel and accommodation</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Arrival</a></li>
+                                            <li><a class="dropdown-item" href="#">VISA Support</a></li>
+                                            <li><a class="dropdown-item" href="#">Overnight stay</a></li>
+                                            <li><a class="dropdown-item" href="#">Trade fair City of</a></li>
+                                            <li><a class="dropdown-item" href="">Barrier-free trade fair visit</a></li>
+                                            <li><a class="dropdown-item" href="">Delegations</a></li>
+
+                                        </ul>
+                                    </div>
+
+
+
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown position-static">
                             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Events</a>
                             <div class="dropdown-menu mega-menu p-4 border-top-0">
                                 <div class="row">
@@ -4244,10 +4316,148 @@
                             </div>
                         </li>
 
-                        <li class="nav-item"><a class="nav-link" href="#">Trade Fair</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Exhibitors 2026</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">For Exhibitors</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Press</a></li>
+                        <li class="nav-item dropdown position-static">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Exhibitors 2026</a>
+                            <div class="dropdown-menu mega-menu p-4 border-top-0">
+                                <div class="row">
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Exhibitor Search</h6>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Product Groups</h6>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Product Index</h6>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Hall Plan</h6>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown position-static">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">For Exhibitors</a>
+                            <div class="dropdown-menu mega-menu p-4 border-top-0">
+                                <div class="row">
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Become an Exhibitor</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Benefits as an exhibitor</a></li>
+                                            <li><a class="dropdown-item" href="#">Exhibitor Application 2026</a></li>
+                                            <li><a class="dropdown-item" href="#">Stand Cost calculation 2026</a></li>
+                                            <li><a class="dropdown-item" href="#">Individual Participation Opportunities</a></li>
+                                            <li><a class="dropdown-item" href="#">Request free stand Areas</a></li>
+                                            <li><a class="dropdown-item" href="#">halal Showcase</a></li>
+                                            <li><a class="dropdown-item" href="#">new product showcase</a></li>
+                                            <li><a class="dropdown-item" href="#">Manage your stand</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Plan trade fari presence on site</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Stand construction</a></li>
+                                            <li><a class="dropdown-item" href="#">Order Deadlines</a></li>
+                                            <li><a class="dropdown-item" href="#">Your Advertising options</a></li>
+                                            <li><a class="dropdown-item" href="#">Service Partner</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Order Services</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Exhibitor passes</a></li>
+                                            <li><a class="dropdown-item" href="#">Construction and dismantling Passes</a></li>
+                                            <li><a class="dropdown-item" href="#">Visitor tickets</a></li>
+                                            <li><a class="dropdown-item" href="">Service shop</a></li>
+                                            <li><a class="dropdown-item" href="">media shop</a></li>
+                                            <li><a class="dropdown-item" href="">Free Advertising material</a></li>
+                                            <li><a class="dropdown-item" href="">Catering</a></li>
+                                            <li><a class="dropdown-item" href="">Lead Management</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Tickets</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Buy Tickets</a></li>
+                                            <li><a class="dropdown-item" href="#">Redeem Ticket Code</a></li>
+                                            <li><a class="dropdown-item" href="#">VIP-Buyer</a></li>
+                                            <li><a class="dropdown-item" href="#">Trade Visitors Definition</a></li>
+                                            <li><a class="dropdown-item" href="">Delegations</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Booth Construction & Dsimantling</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">redeem Codes for construction and dismantling passes</a></li>
+                                            <li><a class="dropdown-item" href="#">set-up and dismantling times</a></li>
+                                            <li><a class="dropdown-item" href="#">Advanced setup</a></li>
+                                            <li><a class="dropdown-item" href="#">Approach for trucks</a></li>
+                                            <li><a class="dropdown-item" href="">Traffic Guidelines</a></li>
+                                            <li><a class="dropdown-item" href="">eSlot</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Digital Features</h6>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown position-static">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Press</a>
+                            <div class="dropdown-menu mega-menu p-4 border-top-0">
+                                <div class="row">
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Press Releases</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Press releases of ISM</a></li>
+                                            <li><a class="dropdown-item" href="#">Press releases of exhibitors</a></li>
+                                            <li><a class="dropdown-item" href="#">Digital Press kit</a></li>
+                                            <li><a class="dropdown-item" href="#">Media partner</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Participation for media representatives</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Journalists and creators</a></li>
+                                            <li><a class="dropdown-item" href="#">Creators</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Press Services on site</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Press Centre</a></li>
+                                            <li><a class="dropdown-item" href="#">Press events</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h6 class="fw-bold">Multimedia</h6>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item" href="#">Image database</a></li>
+                                            <li><a class="dropdown-item" href="#">Video on Demand</a></li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </li>
 
                     </ul>
 
