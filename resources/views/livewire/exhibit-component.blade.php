8752019480a8b891cdce5d6f0bc8db3050cab7e4
kuchnhiarha <exhibitionnetwork@gmail.com>
2025-12-28 22:17:12 +0530

    281220252216

--- resources/views/livewire/exhibit-component.blade.php
+++ resources/views/livewire/exhibit-component.blade.php
@@ -1425,15 +1425,11 @@
         <!-- TITLE -->
         <div>
           <p style="font-size:20px;font-weight:600;margin-bottom:12px;">{{$findevent->eventname}} : Start date | end date | location</p>
-         
-
-        <p style="font-size:20px;font-weight:600;margin-bottom:12px;">
-          Thank you for your interest in exhibiting with us!</p>
-        <p>Your details have been successfully received.</p>
-        <p>Our team will review your submission and share your confirmation and next steps via email shortly.</p>
-
-        <small>If you need any immediate assistance, our Exhibition Help Desk will be happy to support you.</small>
-
+          <p style="font-size:20px;font-weight:600;margin-bottom:12px;">
+              Thank you for your interest in exhibiting with us!</p>
+          <p>Your details have been successfully received.</p>
+          <p>Our team will review your submission and share your confirmation and next steps via email shortly.</p>
+          <small>If you need any immediate assistance, our Exhibition Help Desk will be happy to support you.</small>
         </div>
 
         <!-- CHANNEL + ACTION ROW -->
