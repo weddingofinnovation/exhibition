08d99ac04bb79cf4be53bae4de7e72b114ae7123
kuchnhiarha <exhibitionnetwork@gmail.com>
2026-01-14 23:29:01 +0530

    140120262328

--- resources/views/livewire/admin/admin-event-multi-participants-component.blade.php
+++ resources/views/livewire/admin/admin-event-multi-participants-component.blade.php
@@ -432,6 +432,32 @@
                                     <input type="checkbox" value="1" wire:model="lookingAddParticipants" name="" id=""> Add Participants
                                 </div>
 
+                                <div class="toast-container position-fixed top-0 end-0 p-3">
+
+                                    @if (session()->has('success'))
+                                        <div class="toast align-items-center text-bg-success border-0 show">
+                                            <div class="d-flex">
+                                                <div class="toast-body">
+                                                    ✅ {{ session('success') }}
+                                                </div>
+                                                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
+                                            </div>
+                                        </div>
+                                    @endif
+
+                                    @if (session()->has('warning'))
+                                        <div class="toast align-items-center text-bg-warning border-0 show">
+                                            <div class="d-flex">
+                                                <div class="toast-body">
+                                                    ⚠️ {{ session('warning') }}
+                                                </div>
+                                                <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast"></button>
+                                            </div>
+                                        </div>
+                                    @endif
+
+                                </div>
+
                                 @if($lookingAddParticipants == 1)
                                 <form wire:submit.prevent="updateBrand">
                                     <label class="form-label">Add Participants<span class="text-danger">*</span></label>
