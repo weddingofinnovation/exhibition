106e640debdae0f4e8440e3b4ff0d7e8acd5edf3
kuchnhiarha <exhibitionnetwork@gmail.com>
2026-02-14 10:49:10 +0530

    140220261049

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -1998,9 +1998,8 @@
                       <div class="text-muted fs-sm text-start">
                         {{ucwords(trans(Str::limit($franchise->desc, 24)))}}
                       </div>
-                      <div class="text-muted fs-sm text-start">{{ Str::endsWith(ucwords(trans(Str::limit($franchise->s_desc, 24))), '??') 
-    ? ucwords(trans(Str::limit($franchise->s_desc, 24))) 
-    : ucwords(trans(Str::limit($franchise->s_desc, 24))) . ' ??' }}
+                      <div class="text-muted fs-sm text-start">{{ ucwords(Str::limit($franchise->s_desc ?? '', 24)) }}
+
 </div>
                       @if($category->count() == 0)
 
