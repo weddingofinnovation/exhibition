fbe4ad99e58fb5e27c794937853492a60046775f
kuchnhiarha <exhibitionnetwork@gmail.com>
2026-02-24 18:46:35 +0530

    240220261846

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -7664,7 +7664,7 @@
                               @else
                                     <span class="d-block fw-medium fs-sm">testoo
                                         📞 Call us <a href="tel:+919991856776" class="text-decoration-none text-dark">+91-999-185-6776</a>
-                                    </span>
+                                    </span> {{$getperosnalinfo}}
                             @endif
                         
                         <span class="d-block fw-normal fs-xs text-muted">Get extra ad-ons</span>
