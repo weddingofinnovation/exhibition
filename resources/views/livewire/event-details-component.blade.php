e4d57ff96d8466f2c1840116b982df72041e13a3
kuchnhiarha <exhibitionnetwork@gmail.com>
2026-02-24 14:38:53 +0530

    240220261438

--- resources/views/livewire/event-details-component.blade.php
+++ resources/views/livewire/event-details-component.blade.php
@@ -7546,7 +7546,7 @@
             <div class="text-dark  pl-3 lh-1">
 
             @php 
-               $getperosnalinfo = DB::table('plan_type_customers')->where('event_id', $event->id)->get();
+               $getperosnalinfo = DB::table('plan_type_customers')->where('event_id', $event->id)->first();
             @endphp
 
                 @if($getperosnalinfo->cxtype === 'official')
