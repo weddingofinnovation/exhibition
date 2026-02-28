0e831142805f5bc00ccee6f24c9e665e12c75ba5
kuchnhiarha <exhibitionnetwork@gmail.com>
2026-02-28 20:35:28 +0530

    280220262035

--- resources/views/livewire/exhibitor-invite-component.blade.php
+++ resources/views/livewire/exhibitor-invite-component.blade.php
@@ -147,7 +147,7 @@
                         </div>
 
                         <div class="col-md-6 mb-3">
-                            <label class="form-label">Poster</label>
+                            <label class="form-label">Poser</label>
                             <input type="file" class="form-control" 
                                   wire:model="poster">
                             @error('poster') 
