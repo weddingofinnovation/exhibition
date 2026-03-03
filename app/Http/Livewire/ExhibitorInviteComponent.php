1d507a14e29ccf381b8b3156543ebb3d93d51628
kuchnhiarha <exhibitionnetwork@gmail.com>
2026-03-03 16:19:20 +0530

    030320261619

--- app/Http/Livewire/ExhibitorInviteComponent.php
+++ app/Http/Livewire/ExhibitorInviteComponent.php
@@ -37,7 +37,7 @@ class ExhibitorInviteComponent extends Component
 
     public $grade;
     public $comment;
-    public $referencecode, $search;
+    public $referencecode, $search, $venue;
 
     public function mount($board, $visitorid = null, $event_id = null, $referencecode = null)
     {
